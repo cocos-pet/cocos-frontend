cb9e93ec06938b58df8ecf7469cfaf38315d6549
kong <piorag99@gmail.com>
2025-01-03 06:04:41 +0900

    init: 번들러 절대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@api", replacement: "/src/api" },
+      { find: "@asset", replacement: "/src/asset" },
+      { find: "@common", replacement: "/src/common" },
+      { find: "@page", replacement: "/src/page" },
+      { find: "@route", replacement: "/src/route" },
+      { find: "@shared", replacement: "/src/shared" },
+      { find: "@style", replacement: "/src/style" },
+      { find: "@type", replacement: "/src/type" },
+    ],
+  }
+  
 })
