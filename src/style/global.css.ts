78ed821dbbf067e820ade57725cf882b0cd78cac
Yoonji Lee <rhaxlddbswl3@gmail.com>
2025-01-07 02:31:39 +0900

    init: global.css.ts 설정

---
+++ src/style/global.css.ts
@@ -0,0 +1,13 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle('html',{
+    fontSize:'62.5%,'
+});
+
+globalStyle('body',{
+    margin:0,
+    padding:0,
+    width: '375px',
+    fontFamily:'Pretendard',
+})
+
