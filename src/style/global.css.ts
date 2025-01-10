6791118750ca3132da469fe3ce33f845066ba79e
kong <piorag99@gmail.com>
2025-01-11 04:42:23 +0900

    init: 전역 스타일 옵션 변경

--- src/style/global.css.ts
+++ src/style/global.css.ts
@@ -1,12 +1,20 @@
 import { globalStyle } from "@vanilla-extract/css";
 
 globalStyle("html", {
-  fontSize: "62.5%,",
+  fontSize: "16px", //default
+  "@media": {
+    "(min-width: 450px) and (max-width: 600px)": { fontSize: "13px" },
+    "(max-width: 450px)": { fontSize: "10px" }, //62.5%
+  },
 });
 
 globalStyle("body", {
   margin: 0,
   padding: 0,
-  maxWidth: "375px",
+  minWidth: "375px",
+  maxWidth: "768px",
+  minHeight: "calc(var(--vh, 1vh) * 100)",
   fontFamily: "Pretendard, sans-serif",
+  backgroundColor: "black",
+  color: "red",
 });
