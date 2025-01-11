6056913e74fc0711c76eb8822780a315e845d697
kong <piorag99@gmail.com>
2025-01-11 23:27:11 +0900

    feat: QueryClientProvider 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { RouterProvider } from 'react-router-dom'
-import './App.css'
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./route/Router";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: true, //default
+      },
+    },
+  });
 
   return (
-    <>
-      <RouterProvider router={router}/>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
