131293f644dcaaa46068a1a74f9a16f3410e59e0
kong <piorag99@gmail.com>
2025-01-11 21:51:35 +0900

    feat: api 유틸 함수 작성

--- src/api/index.ts
+++ src/api/index.ts
@@ -20,3 +20,20 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+
+//더욱 빠르게 api 요청을 할 수 있도록 작성된 유틸 함수
+export const get = <T>(...args: Parameters<typeof api.get>) => {
+  return api.get<T>(...args);
+};
+
+export const post = <T>(...args: Parameters<typeof api.post>) => {
+  return api.post<T>(...args);
+};
+
+export const put = <T>(...args: Parameters<typeof api.put>) => {
+  return api.put<T>(...args);
+};
+
+export const del = <T>(...args: Parameters<typeof api.delete>) => {
+  return api.delete<T>(...args);
+};
