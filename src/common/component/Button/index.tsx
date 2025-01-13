e164cfdf8c4881d22400c0ec057fb3dd7c0ea26b
minjeoong <orange_mj@naver.com>
2025-01-13 23:53:42 +0900

    fix: 오타 수정

--- src/common/component/Button/index.tsx
+++ src/common/component/Button/index.tsx
@@ -2,13 +2,14 @@ import clsx from "clsx";
 import React from "react";
 import { button, icon } from "@common/component/Button/styles.css.ts";
 
-interface TextFieldProps {
+interface ButtonProps {
   label?: string;
   leftIcon?: React.ReactNode;
   rightIcon?: React.ReactNode;
   size?: "small" | "medium" | "large";
   variant?: "solidPrimary" | "solidNeutral" | "outlinePrimary";
   disabled?: boolean;
+  onClick?: () => void;
 }
 
 /**
@@ -19,19 +20,25 @@ interface TextFieldProps {
  * @param size 버튼 사이즈 small | medium | large
  * @param variant 버튼 종류 solidPrimary | solidNeutral | outlinePrimary
  * @param disabled 비활성화 여부
+ * @param onClick
  * @constructor minjeoong
  */
 
-export const Button: React.FC<TextFieldProps> = ({
+export const Button: React.FC<ButtonProps> = ({
   label = "Button",
   leftIcon,
   rightIcon,
   size = "medium",
   variant = "solidPrimary",
   disabled,
+  onClick,
 }) => {
   return (
-    <button className={button({ size, variant, disabled })} disabled={disabled}>
+    <button
+      className={button({ size, variant, disabled })}
+      disabled={disabled}
+      onClick={onClick}
+    >
       {leftIcon && <div className={icon}>{leftIcon}</div>}
       {label}
       {rightIcon && <div className={icon}>{rightIcon}</div>}
