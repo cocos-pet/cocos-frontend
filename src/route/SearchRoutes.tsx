4e615f16153884fa017dfedc84a7c813d510a5ca
minjeoong <orange_mj@naver.com>
2025-01-14 16:31:30 +0900

    feat: search 페이지 라우팅 세팅

---
+++ src/route/SearchRoutes.tsx
@@ -0,0 +1,8 @@
+import Search from "@page/search/index/Search.tsx";
+
+const SEARCH_ROUTES = [
+  { path: "/search", element: <Search /> },
+  // { path: "/search/done", element: </> }, // 검색 완료 페이지
+];
+
+export default SEARCH_ROUTES;
