c360491b486bf5f98f527b03e2f61b7e8140ac1d
yarimu <yhli0320@naver.com>
2025-01-14 21:23:36 +0900

    feat: svg 컴포넌트화

--- src/asset/svg/index.ts
+++ src/asset/svg/index.ts
@@ -1,4 +1,6 @@
+export { default as IcNormal } from "./IcNormal";
 export { default as IcDelete } from "./IcDelete";
+export { default as IcLogo } from "./IcLogo";
 export { default as IcTest } from "./IcTest";
 export { default as IcoMessage } from "./IcoMessage";
 export { default as IcoSkeleton } from "./IcoSkeleton";
