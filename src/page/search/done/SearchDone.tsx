db9d5024b840c9a9ee33893ae04b48e51a3ab835
minjeoong <orange_mj@naver.com>
2025-01-14 18:37:11 +0900

    feat: 임시커밋

---
+++ src/page/search/done/SearchDone.tsx
@@ -0,0 +1,39 @@
+import { IcLeftarrow, IcSearch } from "@asset/svg";
+import { TextField } from "@common/component/TextField";
+import React, { ChangeEvent, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { styles } from "@page/search/done/SearchDone.css.ts";
+
+const SearchDone = () => {
+  const [searchParams] = useSearchParams();
+
+  const onSubmit = (searchText: string) => {
+    searchParams.set("searchText", searchText);
+    navigate("/search");
+  };
+
+  const [searchText, setSearchText] = useState("");
+  const navigate = useNavigate();
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.searchHeader}>
+        <IcLeftarrow className={styles.icon} />
+        <TextField
+          value={searchText}
+          placeholder={"검색어를 입력해주세요"}
+          onChange={onChange}
+          onKeyDown={handleKeyDown}
+          icon={<IcSearch onClick={() => onSubmit(searchText)} />}
+          onClearClick={() => setSearchText("")}
+        />
+      </div>
+      <div className={styles.searchContent}></div>
+    </div>
+  );
+};
+
+export default SearchDone;
