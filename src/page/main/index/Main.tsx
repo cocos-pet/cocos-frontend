13cc1f8af6335ab0d2248a8f7ac7013a3e2ece90
minjeoong <orange_mj@naver.com>
2025-01-14 16:12:03 +0900

    fix: main 코드 init

--- src/page/main/index/Main.tsx
+++ src/page/main/index/Main.tsx
@@ -1,33 +1,5 @@
-import Divider from "@common/component/Divider/Divider";
-import Spacing from "@common/component/Spacing/Spacing";
-
-/*
-localhost:5173/main
-*/
-import React, { ChangeEvent, useState } from "react";
-import { Button } from "@common/component/Button";
-import { IcTest } from "@asset/svg";
-import { Toast } from "@common/component/Toast/Toast.tsx";
-
 const Main = () => {
-  return (
-    <>
-      <Toast
-        message={"내용을 입력하세요."}
-        variant={"default"}
-        iconColor={"white"}
-      />
-      {/*<Toast*/}
-      {/*  message={"내용을 입력하세요."}*/}
-      {/*  variant={"error"}*/}
-      {/*  iconColor={"black"}*/}
-      {/*/>*/}
-      <Spacing marginBottom="10" />
-      <Divider size="small" />
-      <Spacing marginBottom="10" />
-      <Divider />
-    </>
-  );
+  return <div>메인</div>;
 };
 
 export default Main;
