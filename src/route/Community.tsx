b4912cfa72a3dc410a4705d89d06ade4fc0b36f2
minjeoong <orange_mj@naver.com>
2025-01-14 20:39:42 +0900

    feat: route 파일 수정

--- src/route/Community.tsx
+++ src/route/Community.tsx
@@ -2,12 +2,13 @@ import Community from "@page/community/index/Community";
 import Post from "@page/community/post/Post";
 import Search from "@page/community/search/Search";
 import Write from "@page/community/write/Write";
+import { PATH } from "@route/path.ts";
 
 const COMMUNITY_ROUTES = [
-    { path: "/community", element: <Community /> },
-    {path: "community/post", element: <Post/>},
-    {path: "community/search", element: <Search/>},
-    {path: "community/write", element: <Write/>},
-  ];
-  
-  export default COMMUNITY_ROUTES;
+  { path: PATH.COMMUNITY.ROOT, element: <Community /> },
+  { path: PATH.COMMUNITY.POST, element: <Post /> },
+  { path: PATH.COMMUNITY.SEARCH, element: <Search /> },
+  { path: PATH.COMMUNITY.WRITE, element: <Write /> },
+];
+
+export default COMMUNITY_ROUTES;
