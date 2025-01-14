a6cc44c43f2ac9d3cca291b0f41d677c5901c1b7
yarimu <yhli0320@naver.com>
2025-01-14 22:41:02 +0900

    feat: 헤더 네브 컴포넌트 구현

--- src/common/component/HeaderNav/HeaderNav.css.ts
+++ src/common/component/HeaderNav/HeaderNav.css.ts
@@ -1,73 +1,61 @@
 import { font, color } from "@style/styles.css.ts";
-import { style } from "@vanilla-extract/css";
 import { recipe, RecipeVariants } from "@vanilla-extract/recipes";
 
-export const HeaderItem = recipe({
+export const headerItem = recipe({
   base: [
     font.heading03,
     {
-      display: "grid", // flex에서 grid로 변경
-      gridTemplateColumns: "repeat(3, 1fr)", // 3개의 영역: 왼쪽, 중간, 오른쪽
-      gridTemplateAreas: `
-        "left center right"
-      `, // 각 영역 이름 지정
+      display: "grid",
+      gridTemplateColumns: "repeat(3, 1fr)",
       alignItems: "center",
+      justifyItems: "center",
+
       width: "37.5rem",
       height: "6.4rem",
       padding: "1.8rem 2.2rem",
+
       backgroundColor: color.gray.gray000,
       color: color.gray.gray900,
     },
   ],
   variants: {
     type: {
-      // 아이콘 + 텍스트 필드 헤더
+      // 아이콘 + 텍스트필드
       field: {
-        gridTemplateColumns: "auto 1fr", // 라벨과 필드를 왼쪽-오른쪽으로 배치
-
+        gridTemplateColumns: "auto 1fr",
         gap: "0.8rem",
       },
 
-      // 아이콘 + 라벨 + x
-      onlyLabel: {
-        justifyItems: "center",
-      },
-
-      //  아이콘 + 라벨 + 아이콘(텍스트버튼)
-      labelBtn: {
-        justifyItems: "center",
-      },
-
-      // 아이콘 + x + 아이콘
-      onlyBtn: {
+      // 아이콘 + 타이틀없음 + 아이콘
+      noTitle: {
         gridTemplateColumns: "repeat(2, 1fr)",
-
-        justifyItems: "center",
       },
     },
   },
 });
 
-// 오른쪽 텍스트 버튼 스타일
-export const rightIconStyle = style([
-  font.body01,
-  {
-    justifySelf: "end",
-
-    color: color.gray.gray600,
-    backgroundColor: "transparent",
-    padding: "0",
-    border: "none",
-    textDecoration: "none",
-    transition: "background-color 0.3s",
-    ":focus": {
-      outline: "none",
+// 오른쪽 버튼_아이콘 또는 텍스트
+export const btnItem = recipe({
+  base: [
+    font.body01,
+    {
+      color: color.gray.gray600,
+      backgroundColor: "transparent",
+      padding: "0",
+      border: "none",
+      textDecoration: "none",
+      transition: "background-color 0.3s",
+      ":focus": {
+        outline: "none",
+      },
+    },
+  ],
+  variants: {
+    side: {
+      left: { justifySelf: "start" },
+      right: { justifySelf: "end" },
     },
   },
-]);
-
-export const leftIconStyle = style({
-  justifySelf: "start",
 });
 
-export type HeaderItemVariant = RecipeVariants<typeof HeaderItem>;
+export type HeaderItemVariant = RecipeVariants<typeof headerItem>;
--- src/common/component/HeaderNav/HeaderNav.tsx
+++ src/common/component/HeaderNav/HeaderNav.tsx
@@ -1,27 +1,30 @@
-// import * as styles from "./HeaderNav.css";
-import { HeaderItemVariant, HeaderItem, leftIconStyle, rightIconStyle } from "./HeaderNav.css";
+import { HeaderItemVariant, headerItem, btnItem } from "./HeaderNav.css";
+
 interface HeaderNavProps {
   leftIcon: React.ReactNode;
-  textField?: React.ReactNode;
-  label?: string;
-  rightIcon?: React.ReactNode;
-  btnText?: React.ReactNode;
+  centerContent?: React.ReactNode;
+  rightBtn?: React.ReactNode;
+  onLeftClick?: () => void;
+  onRightClick?: () => void;
 }
-// type CombinedButtonProps = ButtonProps & Exclude<ButtonVariants, undefined>;
 type CombinedHeaderNavProps = HeaderNavProps & Exclude<HeaderItemVariant, undefined>;
 
-const HeaderNav = ({ textField, label, leftIcon, type, rightIcon, btnText }: CombinedHeaderNavProps) => {
+const HeaderNav = ({ centerContent, leftIcon, rightBtn, type, onLeftClick, onRightClick }: CombinedHeaderNavProps) => {
   return (
-    <div className={HeaderItem({ type })}>
-      {/* {leftIcon && <div className={icon}>{leftIcon}</div>} */}
+    <div className={headerItem({ type })}>
+      {/* 왼쪽 아이콘 영역 */}
+      <button type="button" className={btnItem({ side: "left" })} onClick={onLeftClick}>
+        {leftIcon}
+      </button>
 
-      <div className={leftIconStyle}>{leftIcon}</div>
-      {textField && textField}
-      {label && label}
-      {btnText ? (
-        <div className={rightIconStyle}>{btnText}</div>
-      ) : (
-        rightIcon && <div className={rightIconStyle}>{rightIcon}</div>
+      {/* 중앙 컨텐츠 영역_라벨 또는 텍스트필드 */}
+      {centerContent}
+
+      {/* 오른쪽 버튼 영역_아이콘 또는 텍스트 */}
+      {rightBtn && (
+        <button type="button" className={btnItem({ side: "right" })} onClick={onRightClick}>
+          {rightBtn}
+        </button>
       )}
     </div>
   );
