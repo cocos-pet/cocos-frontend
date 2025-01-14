f2265bb9e4f0b5bdae3ef0c12600f91761df2214
minjeoong <orange_mj@naver.com>
2025-01-14 20:39:33 +0900

    feat: path.ts 생성

---
+++ src/route/path.ts
@@ -0,0 +1,21 @@
+export const PATH = {
+  ONBOARDING: {
+    ROOT: "/onboarding",
+    COMPLETE: "/onboarding/complete",
+  },
+  MAIN: "/main",
+  COMMUNITY: {
+    ROOT: "/community",
+    POST: "/community/post",
+    SEARCH: "/community/search",
+    WRITE: "/community/write",
+  },
+  MYPAGE: {
+    ROOT: "/mypage",
+    MODIFY: "/mypage/modify",
+  },
+  SEARCH: {
+    ROOT: "/search",
+    DONE: "/search/done",
+  },
+};
