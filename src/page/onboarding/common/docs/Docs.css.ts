16d4133074c51e007fb5186b17bbdba7e3b40023
yarimu <yhli0320@naver.com>
2025-01-15 08:26:03 +0900

    design: margin값 변경

--- src/page/onboarding/common/docs/Docs.css.ts
+++ src/page/onboarding/common/docs/Docs.css.ts
@@ -5,13 +5,13 @@ export const titleStyle = recipe({
   base: [
     font.body01,
     {
-      marginTop: "0.8rem",
+      margin: "0.8rem 0 0 0.9rem",
       color: color.red.warning_red200,
     },
   ],
   variants: {
     state: {
-      none: { color: color.gray.gray500 },
+      none: { color: color.gray.gray500, marginLeft: "0" },
       lError: {},
       sError: [font.caption01],
     },
