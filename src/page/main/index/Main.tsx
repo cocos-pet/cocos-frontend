08adcce8543344b46fc9adda5a089bf4ae8c28be
kong <piorag99@gmail.com>
2025-01-14 17:53:33 +0900

    fix: 충돌 처리

--- src/page/main/index/Main.tsx
+++ src/page/main/index/Main.tsx
@@ -1,7 +1,13 @@
+import Comment from "@common/component/Comment/Comment";
 import Nav from "@common/component/Nav/Nav";
 
 const Main = () => {
-  return <Nav />;
+  return (
+    <>
+      <Comment />
+      <Nav />
+    </>
+  );
 };
 
 export default Main;
