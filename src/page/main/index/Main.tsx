f589ef0538fe44d2bf087f42c129e8ce46b87a34
Yoonji Lee <rhaxlddbswl3@gmail.com>
2025-01-15 18:47:31 +0900

    feat: 인기 게시글 스타일 수정

--- src/page/main/index/Main.tsx
+++ src/page/main/index/Main.tsx
@@ -1,7 +1,3 @@
-import Spacing from "@common/component/Spacing/Spacing";
-import { Button } from "@common/component/Button";
-import { IcTest } from "@asset/svg";
-import SimpleBottomSheet from "@common/component/SimpleBottomSheet/SimpleBottomSheet";
 import MainFooter from "./mainFooter/MainFooter";
 import Divider from "@common/component/Divider/Divider";
 import HotPost from "./hotPost/HotPost";
@@ -15,46 +11,11 @@ const postsData = [
   { id: 5, title: "반려동물과 함께하는 여행 준비" },
 ];
 
-const petName = "멍멍이";
+const petName = "";
 
 const Main = () => {
   return (
     <>
-      <div style={{ margin: "3rem", display: "flex", gap: "1rem" }}>
-        <Button label="Button" size="small" variant="solidPrimary" disabled={true} />
-        <Button label="Button" size="small" variant="solidPrimary" disabled={false} />
-        <Button label="Button" size="medium" variant="solidPrimary" disabled={false} />
-        <Button label="Button" size="large" variant="solidPrimary" disabled={false} />
-      </div>
-      <div style={{ margin: "3rem", display: "flex", gap: "1rem" }}>
-        <Button label="Button" size="small" variant="solidNeutral" disabled={true} />
-        <Button label="Button" size="small" variant="solidNeutral" disabled={false} />
-        <Button label="Button" size="medium" variant="solidNeutral" disabled={false} />
-        <Button label="Button" size="large" variant="solidNeutral" disabled={false} />
-      </div>
-      <div style={{ margin: "3rem", display: "flex", gap: "1rem" }}>
-        <Button label="Button" size="small" variant="outlinePrimary" disabled={true} />
-        <Button label="Button" size="small" variant="outlinePrimary" disabled={false} />
-        <Button label="Button" size="medium" variant="outlinePrimary" disabled={false} />
-        <Button label="Button" size="large" variant="outlinePrimary" disabled={false} />
-      </div>
-      <div style={{ margin: "3rem", display: "flex", gap: "1rem" }}>
-        <Button label="Button" leftIcon={<IcTest />} size="medium" variant="solidPrimary" disabled={false} />
-        <Button label="Button" rightIcon={<IcTest />} size="medium" variant="solidPrimary" disabled={false} />
-        <Button
-          label="Button"
-          leftIcon={<IcTest />}
-          rightIcon={<IcTest />}
-          size="medium"
-          variant="solidPrimary"
-          disabled={false}
-        />
-      </div>
-      <span>main</span>
-      <SimpleBottomSheet />
-      <Spacing marginBottom="10" />
-      <Divider size="small" />
-      <Spacing marginBottom="10" />
       <Divider />
 
       <div>
