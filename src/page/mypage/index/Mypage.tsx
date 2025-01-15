b2f42b20304d0bf1006ddd3de91c54b4fd1afd7e
kong <piorag99@gmail.com>
2025-01-16 03:28:47 +0900

    fix: Tab 선택적으로 width 정할 수 있도록 수정

--- src/page/mypage/index/Mypage.tsx
+++ src/page/mypage/index/Mypage.tsx
@@ -1,8 +1,15 @@
 import { Button } from "@common/component/Button";
 import * as styles from "./Mypage.css";
 import Divider from "@common/component/Divider/Divider";
+import Tab from "@common/component/Tab/Tab";
+import { useState } from "react";
+
+
+type ActiveTabType = "review" | "post" | "comment";
 
 const Mypage = () => {
+  const [activeTab, setActiveTab] = useState<ActiveTabType>("review");
+
   return (
     <div style={{ position: "relative", height: "auto" }}>
       <article className={styles.myProfileWrapper}>
@@ -19,7 +26,17 @@ const Mypage = () => {
       <Divider />
 
       <article className={styles.myPageContentWrapper}>
-        <div className={styles.contentHeaderWrapper}></div>
+        <div className={styles.contentHeaderWrapper}>
+          <Tab active={true} width={"12.5rem"}>
+            병원 후기
+          </Tab>
+          <Tab active={true} width={"12.5rem"}>
+            게시글
+          </Tab>
+          <Tab active={true} width={"12.5rem"}>
+            댓글
+          </Tab>
+        </div>
         <div className={styles.contentBody}></div>
       </article>
     </div>
