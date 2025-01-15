68a1d558666dffc3c46e8257a76231f13ce4cf2c
Yoonji Lee <rhaxlddbswl3@gmail.com>
2025-01-16 07:23:16 +0900

    feat: 검색 아이콘 누르면 검색 페이지로 navigate 기능

--- src/page/community/index/Community.tsx
+++ src/page/community/index/Community.tsx
@@ -1,11 +1,18 @@
 import { IcCocos, IcSearch } from "@asset/svg";
 import HeaderNav from "@common/component/HeaderNav/HeaderNav";
 import Nav from "@common/component/Nav/Nav";
+import { useNavigate } from "react-router-dom"; 
 
 const Community = () => {
+  const navigate = useNavigate();
+
+  const handleSearchClick = () => {
+    navigate("/search");
+  };
+
   return (
     <>
-      <HeaderNav leftIcon={<IcCocos />} centerContent="커뮤니티" rightBtn={<IcSearch />} />
+      <HeaderNav leftIcon={<IcCocos />} centerContent="커뮤니티" rightBtn={<IcSearch onClick={handleSearchClick} />} />
       <Nav />
     </>
   );
