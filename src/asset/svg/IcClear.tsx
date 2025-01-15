122197719c026d0fd4141e2f9e675ede14286131
Yoonji Lee <rhaxlddbswl3@gmail.com>
2025-01-16 05:53:23 +0900

    feat: 검색 바 퍼블리싱 완료

--- src/asset/svg/IcClear.tsx
+++ src/asset/svg/IcClear.tsx
@@ -1,13 +1,6 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const SvgIcClear = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={21}
-    height={20}
-    fill="none"
-    {...props}
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" width={21} height={20} fill="none" {...props}>
     <path
       stroke="#717171"
       strokeLinecap="round"
--- src/page/main/index/Main.tsx
+++ src/page/main/index/Main.tsx
@@ -3,6 +3,9 @@ import Divider from "@common/component/Divider/Divider";
 import HotPost from "./hotPost/HotPost";
 import MainHeader from "./mainHeader/mainHeader";
 import Symptom from "./symptom/Symptom";
+import { TextField } from "@common/component/TextField";
+import { useNavigate } from "react-router-dom";
+import { IcSearch } from "@asset/svg";
 
 const postsData = [
   { id: 1, title: "반려동물 건강 관리법" },
@@ -15,9 +18,22 @@ const postsData = [
 const petName = "연진이";
 
 const Main = () => {
+  const navigate = useNavigate();
+
+  const handleSearchClick = () => {
+    navigate("/search");
+  };
+
   return (
     <div style={{ width: "37.5rem" }}>
       <MainHeader />
+      <TextField
+        state="search"
+        placeholder="심장병 백내장"
+        onClick={handleSearchClick}
+        value=""
+        icon={<IcSearch />} 
+      />
       <Divider />
       <Symptom />
       <Divider />
