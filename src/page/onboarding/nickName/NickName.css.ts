21bda98292fc56d8bc0177b2432f2c30cf780148
yarimu <yhli0320@naver.com>
2025-01-16 03:34:01 +0900

    design: 반응형에 맞게 버튼 레이아웃 수정

--- src/page/onboarding/nickName/NickName.css.ts
+++ src/page/onboarding/nickName/NickName.css.ts
@@ -14,12 +14,13 @@ export const imgStyle = style({
 });
 
 export const btnWrapper = style({
+  maxWidth: "76.8rem",
   width: "100%",
   position: "fixed",
   bottom: 0,
 
   display: "grid",
-  gridTemplateColumns: "auto 1fr",
+  gridTemplateColumns: "9.6rem calc(100% - 9.6rem)",
   gap: "1.2rem",
   whiteSpace: "nowrap",
   padding: "1.2rem 2rem 3.2rem 2rem",
--- src/style/global.css.ts
+++ src/style/global.css.ts
@@ -5,8 +5,8 @@ globalStyle("html", {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  
-  fontSize: "16px", //default
+
+  fontSize: "10px", //default
   "@media": {
     "(min-width: 450px) and (max-width: 600px)": { fontSize: "13px" },
     "(max-width: 450px)": { fontSize: "10px" }, //62.5%
