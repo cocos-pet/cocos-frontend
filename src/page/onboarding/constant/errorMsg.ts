8839b747a669925895644fea0e43a63ea6470ebc
yarimu <yhli0320@naver.com>
2025-01-15 18:35:27 +0900

    fix: 에러메시지 수정사항 반영

--- src/page/onboarding/constant/errorMsg.ts
+++ src/page/onboarding/constant/errorMsg.ts
@@ -1,7 +1,6 @@
 export const ERROR_MSG = {
   nickname: {
     duplicate: "이 닉네임은 누군가 사용 중이에요.",
-    koEnNum: "한글, 영문(대소문자), 숫자만 입력할 수 있어요.",
     length: "2자 이상 8자 이하로 입력해주세요.",
     spaceSymbol: "공백이나 특수문자는 사용할 수 없어요.",
   },
