8df4c7b814d1f040971ddfdc0035cad1e051effa
kong <piorag99@gmail.com>
2025-01-17 07:08:03 +0900

    feat: setting navigate 연결

--- src/page/setting/index/Setting.tsx
+++ src/page/setting/index/Setting.tsx
@@ -2,18 +2,25 @@ import { IcChevronLeft, IcEditPen, IcOut } from "@asset/svg";
 import HeaderNav from "@common/component/HeaderNav/HeaderNav";
 import * as styles from "./Setting.css";
 import Divider from "@common/component/Divider/Divider";
+import { useNavigate } from "react-router-dom";
 
 const Setting = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <HeaderNav leftIcon={<IcChevronLeft width={20} height={20} />} centerContent={"설정"} />
+      <HeaderNav
+        leftIcon={<IcChevronLeft width={20} height={20} />}
+        centerContent={"설정"}
+        onLeftClick={() => navigate("/mypage")}
+      />
 
       <div className={styles.settingWrapper}>
         <div className={styles.myprofileSettingWrapper}>
           <span className={styles.myprofileText}>마이 프로필</span>
           <Divider size={"small"} />
           <div className={styles.editMyProfile}>
-            <span className={styles.myProfileSpan}>
+            <span className={styles.myProfileSpan} onClick={() => navigate("/setting/edit-profile")}>
               <IcEditPen width={20} height={20} />
               <span className={styles.myProfileSpanText}>내 정보수정</span>
             </span>
@@ -24,7 +31,7 @@ const Setting = () => {
           <span className={styles.myprofileText}>계정</span>
           <Divider size={"small"} />
           <div className={styles.editMyProfile}>
-            <span className={styles.myProfileSpan}>
+            <span className={styles.myProfileSpan} onClick={() => alert("로그아웃 구현 예정!")}>
               <IcOut width={20} height={20} />
               <span className={styles.myProfileSpanText}>로그아웃</span>
             </span>
