a7c8508420de1a485b2954a2b07a191d097eb0d5
Yoonji Lee <rhaxlddbswl3@gmail.com>
2025-01-16 23:10:29 +0900

    feat: 플로팅 버튼 navigate 추가

--- src/common/component/FloatingBtn/Floating.tsx
+++ src/common/component/FloatingBtn/Floating.tsx
@@ -1,5 +1,6 @@
 import * as styles from "./Floating.css";
 import { Plus } from "@asset/svg";
+import { useNavigate } from "react-router-dom"; // react-router-dom의 useNavigate 훅 가져오기
 
 interface FloatingBtnProps {
   disabled?: boolean;
@@ -7,10 +8,20 @@ interface FloatingBtnProps {
 }
 
 function FloatingBtn({ disabled = false, onClick }: FloatingBtnProps) {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+    if (!disabled) {
+      navigate("/community/write");
+    }
+  };
+
   const floatingBtn = `${styles.floatingBtnBase} ${disabled ? styles.floatingBtnDisabled : styles.floatingBtnEnabled}`;
 
   return (
-    <button className={floatingBtn} onClick={onClick} disabled={disabled}>
+    <button className={floatingBtn} onClick={handleClick} disabled={disabled}>
       <Plus style={{ width: "2.4rem" }} />
     </button>
   );
