929118c1a4fdea6fa73f99192662fc01bed1d669
yarimu <yhli0320@naver.com>
2025-01-16 20:23:00 +0900

    fix: button 컴포넌트 active css 수정 (민정커밋)

--- src/common/component/Button/styles.css.ts
+++ src/common/component/Button/styles.css.ts
@@ -44,11 +44,12 @@ export const button = recipe({
           color: "rgb(244, 244, 244)",
         },
         ":focus": {
-          backgroundColor: "rgb(59, 183, 228)",
+          backgroundColor: "rgb(58, 177, 220)",
           color: "rgb(237, 237, 237)",
         },
         ":active": {
-          backgroundColor: "rgb(58, 177, 220)",
+          backgroundColor: "rgb(59, 183, 228)",
+
           color: "rgb(58, 177, 220)",
         },
       },
@@ -60,11 +61,11 @@ export const button = recipe({
           color: "rgb(109, 109, 109)",
         },
         ":focus": {
-          backgroundColor: "rgb(231, 231, 231)",
+          backgroundColor: "rgb(222, 222, 222)",
           color: "rgb(107, 107, 107)",
         },
         ":active": {
-          backgroundColor: "rgb(222, 222, 222)",
+          backgroundColor: "rgb(231, 231, 231)",
           color: "rgb(104, 104, 104)",
         },
       },
@@ -78,17 +79,16 @@ export const button = recipe({
           border: `0.1rem solid ${color.primary.blue500}`,
         },
         ":active": {
-          backgroundColor: "rgba(67, 214, 255, 0.16)",
+          backgroundColor: "rgba(67, 214, 255, 08)",
           color: "rgba(39, 63, 69, 1)",
           border: `0.1rem solid ${color.primary.blue500}`,
         },
         ":focus": {
-          backgroundColor: "rgba(67, 214, 255, 0.08)",
+          backgroundColor: "rgba(67, 214, 255, 0.16)",
           color: "rgba(33, 46, 50, 1)",
           border: `0.1rem solid ${color.primary.blue500}`,
         },
       },
-      
     },
     disabled: {
       true: {
