4ebbafc9b0c15b0db5ba7096c4ae7baf4499e075
minjeoong <orange_mj@naver.com>
2025-01-16 23:17:39 +0900

    feat: 게시물 생성 페이지 헤더 구성

---
+++ src/page/community/write/Write.css.ts
@@ -0,0 +1,6 @@
+import { createVar, style } from "@vanilla-extract/css";
+import { color, font } from "@style/styles.css.ts";
+
+export const writeWrap = style({
+  backgroundColor: color.gray.gray200,
+});
--- src/page/community/write/Write.tsx
+++ src/page/community/write/Write.tsx
@@ -1,8 +1,27 @@
+import HeaderNav from "@common/component/HeaderNav/HeaderNav";
+import {
+  IcClear,
+  IcDelete,
+  IcDeleteBlack,
+  IcLeftarrow,
+  React,
+} from "@asset/svg";
+import { writeWrap } from "@page/community/write/Write.css.ts";
 
 const Write = () => {
-  return (
-    <div>Write</div>
-  )
-}
+  const onBackClick = () => {};
 
-export default Write
+  return (
+    <div>
+      <HeaderNav
+        leftIcon={<IcDeleteBlack width={24} />}
+        onLeftClick={onBackClick}
+        // type={"field"}
+        centerContent={"글쓰기"}
+      />
+      <div className={writeWrap}></div>
+    </div>
+  );
+};
+
+export default Write;
