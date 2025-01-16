70f6104af961d222068e7e7c4fdb000748db68d8
yarimu <yhli0320@naver.com>
2025-01-17 08:49:56 +0900

    feat: 활성화 구현

--- src/page/onboarding/index/component/petGender/PetGender.tsx
+++ src/page/onboarding/index/component/petGender/PetGender.tsx
@@ -5,8 +5,17 @@ import Title from "@page/onboarding/index/common/title/Title";
 import Docs from "@page/onboarding/index/common/docs/Docs";
 import DualOptionSelector from "../dualOptionSelector/DualOptionSelector";
 import { Button } from "@common/component/Button";
+import { data } from "@page/onboarding/index/constant/genderData";
+import { useState } from "react";
 
 const PetGender = () => {
+  // 선택 상태 관리 (초기값: null)
+  const [gender, setGender] = useState<string | null>(null);
+
+  // 선택한 버튼 텍스트 = selectedPetType
+  const handleOptionSelect = (value: string) => {
+    setGender(value);
+  };
   // 뒤로 가기
   const navigate = useNavigate();
   const handleGoBack = () => {
@@ -26,12 +35,12 @@ const PetGender = () => {
           <Docs text={ONBOARDING_GUIDE.petGender.docs} />
         </div>
         {/* 성별 선택 영역 */}
-        <DualOptionSelector leftLabel="암컷" rightLabel="수컷" />
+        <DualOptionSelector data={data.gender} onSelect={handleOptionSelect} />
       </div>
       {/* 하단 영역 */}
       <div className={styles.btnWrapper}>
         <Button label="돌아가기" size="large" variant="solidNeutral" disabled={false} onClick={handleGoBack} />
-        <Button label="다음" size="large" variant="solidPrimary" disabled={false} onClick={handleNext} />
+        <Button label="다음" size="large" variant="solidPrimary" disabled={!gender} onClick={handleNext} />
       </div>
     </>
   );
