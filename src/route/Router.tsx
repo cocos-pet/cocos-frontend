e4a8f0ba6a3808f10a6bd6fb441d8000275865be
yarimu <yhli0320@naver.com>
2025-01-17 05:32:24 +0900

    fix: 오타 수정

--- src/route/Router.tsx
+++ src/route/Router.tsx
@@ -2,12 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import COMMUNITY_ROUTES from "./Community";
 import MAIN_ROUTES from "./MainRoutes";
 import MYPAGE_ROUTES from "./MyPageRoutes";
-import ONBOARGING_ROUTES from "./OnboardingRoutes";
+import ONBOARDING_ROUTES from "./OnboardingRoutes";
 
 //children은 Outlet에서 사용하는 방식 (레이아웃 지정해야할 때)
 const router = createBrowserRouter([
   { path: "/", element: <>just home</> },
-  ...ONBOARGING_ROUTES,
+  ...ONBOARDING_ROUTES,
   ...MAIN_ROUTES,
   ...COMMUNITY_ROUTES,
   ...MYPAGE_ROUTES,
