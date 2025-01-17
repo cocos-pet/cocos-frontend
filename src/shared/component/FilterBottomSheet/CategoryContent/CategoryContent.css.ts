573cea544218a15e8db279b95214b481a29355b5
minjeoong <orange_mj@naver.com>
2025-01-18 05:07:11 +0900

    feat: x 버튼 안 보이는 것 살리기

--- src/shared/component/FilterBottomSheet/CategoryContent/CategoryContent.css.ts
+++ src/shared/component/FilterBottomSheet/CategoryContent/CategoryContent.css.ts
@@ -3,7 +3,6 @@ import { style } from "@vanilla-extract/css";
 export const styles = {
   kindWrapper: style({
     display: "flex",
-    width: "33.5rem",
     padding: "16px 0px",
     flexDirection: "column",
     alignItems: "flex-start",
