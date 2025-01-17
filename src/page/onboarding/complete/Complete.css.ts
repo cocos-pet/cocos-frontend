e586c70a12f7ccad9f1cd21d4ad79db5c66d020e
yarimu <yhli0320@naver.com>
2025-01-18 03:30:46 +0900

    feat: 완성뷰 초기 구조

---
+++ src/page/onboarding/complete/Complete.css.ts
@@ -0,0 +1,28 @@
+import { style } from "@vanilla-extract/css";
+
+export const layout = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "3.2rem",
+  margin: "7.2rem 2rem 6rem 2rem",
+});
+export const titleWrapper = style({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+  gap: "0.8rem",
+});
+export const btnWrapper = style({
+  maxWidth: "76.8rem",
+  width: "100%",
+  position: "fixed",
+  bottom: 0,
+  display: "grid",
+  gridTemplateColumns: "9.6rem calc(100% - 10.8rem)",
+  gap: "1.2rem",
+  whiteSpace: "nowrap",
+  padding: "1.2rem 2rem 3.2rem 2rem",
+});
--- src/page/onboarding/complete/Complete.tsx
+++ src/page/onboarding/complete/Complete.tsx
@@ -1,8 +1,37 @@
+import * as styles from "./Complete.css";
+import { useNavigate } from "react-router-dom";
+import Docs from "../index/common/docs/Docs";
+import Title from "../index/common/title/Title";
+import { Button } from "@common/component/Button";
 
 const Complete = () => {
+  // 뒤로 가기 (추후 플로우에 맞게 구현)
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+  // 다음 버튼
+  const handleNext = () => {
+    navigate("/register-pet");
+  };
   return (
-    <div>Complete</div>
-  )
-}
+    <>
+      <div className={styles.layout}>
+        <div className={styles.titleWrapper}>
+          <div>
+            <Title text={"@@@님!"} />
+            <Title text={"반려동물을 등록해 주세요."} />
+          </div>
+          <Docs text={"작은 정보가 우리 아이의 건강을 지켜요 !"} />
+        </div>
+        <img src={img} alt="onboarding-character" width={296} height={325} />
+      </div>
+      <div className={styles.btnWrapper}>
+        <Button label="돌아가기" size="large" variant="solidNeutral" disabled={false} onClick={handleGoBack} />
+        <Button label="다음" size="large" variant="solidPrimary" disabled={false} onClick={handleNext} />
+      </div>
+    </>
+  );
+};
 
-export default Complete
+export default Complete;
