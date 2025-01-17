c480710b2511f52ce31fc62ec24ee2139dd1e3cf
kong <piorag99@gmail.com>
2025-01-17 09:19:35 +0900

    feat: 로그아웃 모달 연결

--- src/page/setting/index/Setting.tsx
+++ src/page/setting/index/Setting.tsx
@@ -3,9 +3,13 @@ import HeaderNav from "@common/component/HeaderNav/HeaderNav";
 import * as styles from "./Setting.css";
 import Divider from "@common/component/Divider/Divider";
 import { useNavigate } from "react-router-dom";
+import SimpleBottomSheet from "@common/component/SimpleBottomSheet/SimpleBottomSheet";
+import useSimpleBottomSheet from "@shared/hook/useSimpleBottomSheet";
 
+//todo: 로그아웃 api 연결 필요
 const Setting = () => {
   const navigate = useNavigate();
+  const { isOpen, openBottomSheet, closeBottomSheet } = useSimpleBottomSheet();
 
   return (
     <>
@@ -31,13 +35,24 @@ const Setting = () => {
           <span className={styles.myprofileText}>계정</span>
           <Divider size={"small"} />
           <div className={styles.editMyProfile}>
-            <span className={styles.myProfileSpan} onClick={() => alert("로그아웃 구현 예정!")}>
+            <span className={styles.myProfileSpan} onClick={() => openBottomSheet()}>
               <IcOut width={20} height={20} />
               <span className={styles.myProfileSpanText}>로그아웃</span>
             </span>
           </div>
         </div>
       </div>
+
+      <SimpleBottomSheet
+        isOpen={isOpen}
+        handleClose={closeBottomSheet}
+        content="로그아웃 하시겠어요?"
+        subContent="언제든 다시 오시면 기다리고 있을게요!"
+        leftText="취소"
+        rightText="로그아웃"
+        leftOnClick={() => closeBottomSheet()}
+        rightOnClick={() => alert("로그아웃 구현 예정")}
+      />
     </>
   );
 };
