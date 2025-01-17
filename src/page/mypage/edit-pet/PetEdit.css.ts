20ba4b565904a1402378eae0ba3d07158f78e12a
kong <piorag99@gmail.com>
2025-01-18 05:26:28 +0900

    feat: 반려동물 정보 수정 뷰 퍼블리싱

---
+++ src/page/mypage/edit-pet/PetEdit.css.ts
@@ -0,0 +1,148 @@
+import { color, font, semanticColor } from "@style/styles.css";
+import { style } from "@vanilla-extract/css";
+
+export const petEditWrapper = style({
+  display: "flex",
+  width: "37.5rem",
+  padding: "20px 0px 72px 0px",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "3.2rem",
+});
+
+export const profileInfo = style({
+  display: "flex",
+  padding: "0rem 2rem",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "1.2rem",
+  alignSelf: "stretch",
+});
+
+export const defaultInfo = style({
+  display: "flex",
+  padding: "0rem 2rem",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  gap: "1.2rem",
+  alignSelf: "stretch",
+});
+
+export const knownSymptoms = style({
+  display: "flex",
+  padding: "0rem 2rem",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  gap: "1.2rem",
+  alignSelf: "stretch",
+});
+
+export const knownDisease = style({
+  display: "flex",
+  padding: "0rem 2rem",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  gap: "1.2rem",
+  alignSelf: "stretch",
+});
+
+export const profileImage = style({
+  width: "7.2rem",
+  height: "7.2rem",
+  display: "flex",
+  alignItems: "center",
+  gap: "1.6rem",
+
+  borderRadius: "999px",
+  border: `1px solid ${semanticColor.line.strong}`,
+  background: color.gray.gray100,
+});
+
+export const nicknameWrapper = style([
+  font.heading02,
+  {
+    display: "flex",
+    alignItems: "center",
+    gap: "0.4rem",
+
+    color: semanticColor.text.normal,
+  },
+]);
+
+export const defaultText = style([
+  font.label01,
+  {
+    alignSelf: "stretch",
+    color: semanticColor.text.assistive,
+  },
+]);
+
+export const defaultInfoListWrapper = style({
+  display: "flex",
+  width: "33.5rem",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  gap: "2rem",
+});
+
+export const defaultInfoList = style({
+  display: "flex",
+  alignItems: "center",
+  gap: "1.6rem",
+  alignSelf: "stretch",
+});
+
+export const defaultInfoListLeft = style([
+  font.body01,
+  {
+    width: "10rem",
+    color: semanticColor.text.assistive,
+  },
+]);
+
+export const defaultInfoListRight = style([
+  font.body01,
+  {
+    display: "flex",
+    padding: "0.6rem 0rem",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    gap: "0.6rem",
+    flex: "1 0 0",
+
+    color: semanticColor.text.normal,
+  },
+]);
+
+export const chipContainer = style({
+  display: "flex",
+  alignItems: "center",
+  alignContent: "center",
+  gap: "1.2rem",
+  alignSelf: "stretch",
+  flexWrap: "wrap",
+});
+
+export const nameInput = style([
+  font.heading02,
+  {
+    display: "flex",
+    alignItems: "center",
+    gap: "0.4rem",
+
+    color: semanticColor.text.normal,
+
+    border: "none",
+    borderBottom: "1px solid #ccc",
+    padding: "0.2rem 0",
+    outline: "none",
+
+    ":focus": { borderBottom: `1px solid ${semanticColor.primary.normal}` },
+  },
+]);
+
+export const errorLayout = style({
+  marginTop: "0.8rem",
+});
--- src/page/mypage/edit-pet/PetEdit.tsx
+++ src/page/mypage/edit-pet/PetEdit.tsx
@@ -1,10 +1,128 @@
-import { IcChevronLeft } from "@asset/svg";
+import { IcChevronLeft, IcChevronRight, IcEditPen } from "@asset/svg";
 import HeaderNav from "@common/component/HeaderNav/HeaderNav";
+import { PATH } from "@route/path";
+import { useNavigate } from "react-router-dom";
+import * as styles from "./PetEdit.css";
+import Divider from "@common/component/Divider/Divider";
+import { Button } from "@common/component/Button";
+import { useEffect, useState } from "react";
+import Docs from "@page/onboarding/index/common/docs/Docs";
+import { validateNickname } from "@page/onboarding/index/util/validateNickname";
+
+const DEFAULT_TYPE = ["종류", "세부 종류", "성별", "나이"] as const;
 
 const PetEdit = () => {
+  const navigate = useNavigate();
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState("포리");
+  const [validationMessages, setValidationMessages] = useState<string[]>([]);
+  const [isValid, setIsVaild] = useState(false);
+
+  useEffect(() => {
+    if (name) {
+      const inVaildateMessages = validateNickname(name);
+      setValidationMessages(inVaildateMessages); // 유효성 검사 메시지 업데이트
+      setIsVaild(inVaildateMessages.length === 0); // 유효성 상태 설정
+    }
+  }, [name]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleInputBlur = () => {
+    if (isValid) {
+      setIsEditing(false);
+    }
+  };
+
+  const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && isValid) {
+      setIsEditing(false);
+    }
+  };
+
   return (
     <div>
-      <HeaderNav leftIcon={<IcChevronLeft width={24} height={24} />} centerContent={"반려동물 정보 수정"} />
+      <HeaderNav
+        leftIcon={<IcChevronLeft width={24} height={24} />}
+        centerContent={"반려동물 정보 수정"}
+        onLeftClick={() => navigate(PATH.MYPAGE.ROOT)}
+      />
+      <section className={styles.petEditWrapper}>
+        <article className={styles.profileInfo}>
+          <img className={styles.profileImage} alt="프로필 이미지" />
+          <span className={styles.nicknameWrapper}>
+            {isEditing ? (
+              <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+                <input
+                  className={styles.nameInput}
+                  value={name}
+                  onChange={handleInputChange}
+                  onBlur={handleInputBlur}
+                  onKeyDown={handleInputKeyDown}
+                />
+                <div className={styles.errorLayout}>
+                  {validationMessages.map((message) => (
+                    <Docs key={`error-${message}`} state="lError" text={message} />
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <>
+                {name}
+                <IcEditPen width={24} height={24} onClick={handleEditClick} style={{ cursor: "pointer" }} />
+              </>
+            )}
+          </span>
+        </article>
+
+        <article className={styles.defaultInfo}>
+          <span className={styles.defaultText}>기본 정보</span>
+          <Divider size="small" />
+          <div className={styles.defaultInfoListWrapper}>
+            {DEFAULT_TYPE.map((item) => (
+              <div key={`default-type-${item}`} className={styles.defaultInfoList}>
+                <span className={styles.defaultInfoListLeft}>{item}</span>
+                <button className={styles.defaultInfoListRight}>
+                  강아지
+                  <IcChevronRight width={20} height={20} />
+                </button>
+              </div>
+            ))}
+          </div>
+        </article>
+        <article className={styles.knownDisease}>
+          <span className={styles.defaultText}>앓고있는/관심있는 질병</span>
+          <Divider size="small" />
+          <div className={styles.chipContainer}></div>
+          <span style={{ width: "10.2rem" }}>
+            <Button
+              variant={"solidNeutral"}
+              leftIcon={<IcEditPen width={20} height={20} />}
+              label={"수정하기"}
+              size="small"
+            />
+          </span>
+        </article>
+        <article className={styles.knownSymptoms}>
+          <span className={styles.defaultText}>앓고있는/관심있는 증상</span>
+          <Divider size="small" />
+          <div className={styles.chipContainer}></div>
+          <span style={{ width: "10.2rem" }}>
+            <Button
+              variant={"solidNeutral"}
+              leftIcon={<IcEditPen width={20} height={20} />}
+              label={"수정하기"}
+              size="small"
+            />
+          </span>
+        </article>
+      </section>
     </div>
   );
 };
