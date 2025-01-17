b2977b27bcd76323109d4e1cb19abe39439671fa
Yoonji Lee <rhaxlddbswl3@gmail.com>
2025-01-18 03:28:45 +0900

    fix: 반응형

--- src/page/community/index/Community.css.ts
+++ src/page/community/index/Community.css.ts
@@ -1,7 +1,7 @@
 import { style } from "@vanilla-extract/css";
 
 export const communityContainer = style({
-  width: "37.5rem",
+  width: "100%",
   margin: "0 auto",
   position: "relative",
 });
@@ -9,7 +9,7 @@ export const communityContainer = style({
 export const communityHeader = style({
   position: "fixed",
   top: 0,
-  width: "37.5rem",
+  width: "100%",
   backgroundColor: "#fff",
   height: "6.4rem",
   zIndex: 1,
@@ -24,7 +24,7 @@ export const communityFooter = style({
   bottom: 0,
   zIndex: 10,
   backgroundColor: "#fff",
-  width: "37.5rem",
+  width: "100%",
 });
 
 export const postContainer = style({
