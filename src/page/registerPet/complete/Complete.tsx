22d01e9b478c5d42b0db906115da45b4f8b9630b
yarimu <yhli0320@naver.com>
2025-01-18 05:16:56 +0900

    feat: 반려동물 완료 뷰 구현

--- src/page/registerPet/complete/Complete.tsx
+++ src/page/registerPet/complete/Complete.tsx
@@ -1,10 +1,34 @@
+import * as styles from "./Complete.css";
+import { useNavigate } from "react-router-dom";
+import Docs from "@page/onboarding/index/common/docs/Docs";
+import Title from "@page/onboarding/index/common/title/Title";
+import { Button } from "@common/component/Button";
+import { IcComplete } from "@asset/svg";
 
 const Complete = () => {
-  return (
-    <div>
-      반려동물 컴플리트
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const handleSkip = () => {
+    navigate("/main");
+  };
 
-export default Complete
+  return (
+    <div className={styles.backGround}>
+      <div className={styles.layout}>
+        <div className={styles.titleWrapper}>
+          <div>
+            <Title text={"@@@님"} />
+            <Title text={"등록이 완료되었습니다"} />
+          </div>
+          <Docs text={"반려동물과 함께하는 긴 시간, 더 잘 돌봐드릴게요"} />
+        </div>
+        <IcComplete />
+      </div>
+
+      <div className={styles.btnWrapper}>
+        <Button label="동물 등록 끝내기" size="large" variant="solidPrimary" disabled={false} onClick={handleSkip} />
+      </div>
+    </div>
+  );
+};
+
+export default Complete;
