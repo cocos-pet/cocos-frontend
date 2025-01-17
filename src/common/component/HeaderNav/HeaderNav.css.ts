95ff33320797b8c4ba5061c442003fb892ac05c7
yarimu <yhli0320@naver.com>
2025-01-18 02:36:00 +0900

    feat: build 에러 해결

--- src/common/component/HeaderNav/HeaderNav.css.ts
+++ src/common/component/HeaderNav/HeaderNav.css.ts
@@ -14,9 +14,9 @@ export const headerItem = recipe({
       height: "6.4rem",
       padding: "1.8rem 2.2rem",
 
+      borderBottom: `1px solid ${color.gray.gray200}`,
       backgroundColor: color.gray.gray000,
       color: color.gray.gray900,
-      borderBottom: `1px solid ${color.gray.gray200}`,
     },
   ],
   variants: {
