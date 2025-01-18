43c76965c8b5cd7501cab95afba88d13b36a3db1
kong <piorag99@gmail.com>
2025-01-19 05:58:50 +0900

    feat: meber api 주소값 상수 추가

--- src/api/domain/mypage/edit-pet/index.ts
+++ src/api/domain/mypage/edit-pet/index.ts
@@ -1,5 +1,8 @@
+import { API_PATH } from "@api/constants/apiPath";
+import { get } from "@api/index";
 import { paths } from "@type/schema";
 
 export const getMemberInfo = async () => {
-  type getMemberInfoRequest = paths["/api/dev/members"]["get"]["requestBody"];
+  //type getMemberInfoRequest = paths["/api/dev/members"]["get"]["requestBody"];
+  return await get(API_PATH.)
 };
