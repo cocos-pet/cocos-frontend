7d121bce4bbca5d6506b42c023d681089496e9b5
minjeoong <orange_mj@naver.com>
2025-01-18 09:08:27 +0900

    feat: type 만 작성

--- src/api/domain/community/search/index.ts
+++ src/api/domain/community/search/index.ts
@@ -1,18 +1,9 @@
-//아래는 예시입니다.
-//만약 스키마 사용 못할 경우, 아래와 같이 필요한 request body 타입 정의하기 (+ response 인터페이스도 정의하기)
-export interface PostCommentWithMentionRequest {
-  orgIds: number[] | null;
-  content: string;
-  postId: number;
-}
+import { paths } from "src/type/schema";
 
-export interface MeetingPeopleResponse {
-  requestBody: PostCommentWithMentionRequest;
-}
+type searchGetResponse =
+  paths["/api/dev/search"]["get"]["responses"]["200"]["content"]["*/*"];
 
-//⭐️!!스키마는 paths로 사용하기로 약속!!⭐️
-//import { paths } from "src/__generated__/schema;
-// type Post = paths['/post/v2']['get']['responses']['200']['content']['application/json;charset=UTF-8']['posts'];
+type searchPostRequest = paths["/api/dev/search"]["post"]["requestBody"];
 
 //아래와 같이 requset용 타입 , response용 타입으로 사용하는게 당장은 편해도 장기적인 유지 보수에는 불리.
 //따라서 아래와 같은 방식은 사용 x
