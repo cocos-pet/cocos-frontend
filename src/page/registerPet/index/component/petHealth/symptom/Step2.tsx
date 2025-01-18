5f766b4a83e91a7e4cdfd102a2a7c515cd9d257b
yarimu <yhli0320@naver.com>
2025-01-18 13:56:52 +0900

    feat: 활성화 로직 구현 (UI 반영은x)

--- src/page/registerPet/index/component/petHealth/symptom/Step2.tsx
+++ src/page/registerPet/index/component/petHealth/symptom/Step2.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"; // useState import
 import * as styles from "./Step2.css";
 
 import Docs from "@page/onboarding/index/common/docs/Docs";
@@ -5,9 +6,22 @@ import Title from "@page/onboarding/index/common/title/Title";
 import { GOGO, BodyPart } from "./constant";
 import { Button } from "@common/component/Button";
 import Chip from "@common/component/Chip/Chip";
-import Spacing from "@common/component/Spacing/Spacing";
 
 const Step2 = () => {
+  const [selectedDiseases, setSelectedDiseases] = useState<number[]>([]); // 선택된 disease.id를 관리
+
+  const handleChipClick = (diseaseId: number) => {
+    setSelectedDiseases((prevSelected) => {
+      // 질병이 이미 선택된 경우 제거
+      if (prevSelected.includes(diseaseId)) {
+        return prevSelected.filter((id) => id !== diseaseId);
+      }
+
+      // 7개 미만일 때만 선택 가능
+      return prevSelected.length < 7 ? [...prevSelected, diseaseId] : prevSelected;
+    });
+  };
+
   const handleGoBack = () => {
     console.log("다음 pr에서 구현할래욥.");
   };
@@ -15,6 +29,7 @@ const Step2 = () => {
   const handleNext = () => {
     console.log("다음 pr에서 구현할래욥.");
   };
+
   return (
     <>
       {/* 상단 영역 */}
@@ -30,19 +45,23 @@ const Step2 = () => {
             <span>{body.name}</span>
             <div className={styles.chipLayout}>
               {body.diseases.map((disease) => (
-                <Chip key={disease.id} label={disease.name} />
+                <Chip key={disease.id} label={disease.name} onClick={() => handleChipClick(disease.id)} />
               ))}
             </div>
           </div>
         ))}
-        <Spacing marginBottom="11.4" />
       </div>
 
       {/* 하단 영역 */}
-
       <div className={styles.btnWrapper}>
         <Button label="돌아가기" size="large" variant="solidNeutral" disabled={false} onClick={handleGoBack} />
-        <Button label="다음" size="large" variant="solidPrimary" disabled={false} onClick={handleNext} />
+        <Button
+          label="다음"
+          size="large"
+          variant="solidPrimary"
+          disabled={selectedDiseases.length === 0 || selectedDiseases.length > 7} // 0개 또는 7개 이상일 때 비활성화
+          onClick={handleNext}
+        />
       </div>
     </>
   );
