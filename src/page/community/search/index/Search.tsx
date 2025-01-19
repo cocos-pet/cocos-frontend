8b73fa762fd461a08594b3af22b62f66406a1eee
minjeoong <orange_mj@naver.com>
2025-01-20 08:09:34 +0900

    feat: 중복 호출 잡는중

--- src/page/community/search/index/Search.tsx
+++ src/page/community/search/index/Search.tsx
@@ -26,6 +26,7 @@ const Search = () => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const onSubmit = (searchText: string) => {
+    if (!searchText || isLoading) return;
     mutate(
       { keyword: searchText },
       {
