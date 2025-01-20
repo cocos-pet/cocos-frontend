5f0e2739cdf23b6b074e81f0695d331641367a4a
minjeoong <orange_mj@naver.com>
2025-01-21 05:50:13 +0900

    refector: 쿼리키 파라미터 제거

--- src/api/domain/community/search/hook.ts
+++ src/api/domain/community/search/hook.ts
@@ -1,10 +1,14 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { SEARCH_QUERY_KEY } from "@api/domain/community/post/hook.ts";
 import {
   getSearch,
   postSearch,
   searchPostType,
 } from "@api/domain/community/search/index.ts";
+
+const SEARCH_QUERY_KEY = {
+  SEARCH_QUERY_KEY: () => ["search"],
+  SEARCH_POST_QUERY_KEY: () => ["searchPost"],
+};
 
 /**
  * @description 최근 검색어 조회 API
@@ -21,14 +25,14 @@ export const useSearchGet = () => {
 /**
  * @description 최근 검색어 입력 API
  */
-export const useSearchPost = (params: searchPostType) => {
+export const useSearchPost = () => {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationKey: SEARCH_QUERY_KEY.SEARCH_POST_QUERY_KEY(params.keyword),
+    mutationKey: SEARCH_QUERY_KEY.SEARCH_POST_QUERY_KEY(),
     mutationFn: (params: searchPostType) => postSearch(params),
     onSuccess: (data, variables) => {
       queryClient.invalidateQueries({
-        queryKey: SEARCH_QUERY_KEY.SEARCH_POST_QUERY_KEY(params.keyword),
+        queryKey: SEARCH_QUERY_KEY.SEARCH_POST_QUERY_KEY(),
       });
     },
   });
