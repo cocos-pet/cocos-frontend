a7bf21c07e44ef9a6f72f6c2e5bbb40cc2f7ed81
yarimu <yhli0320@naver.com>
2025-01-22 07:02:39 +0900

    feat: 반려동물 등록 api 함수 작성

---
+++ src/api/domain/register-pet/pets/index.ts
@@ -0,0 +1,19 @@
+import { API_PATH } from "@api/constants/apiPath";
+import { post } from "@api/index";
+import { paths } from "@type/schema";
+
+// 반려동물 등록 POST API
+export type myPetPostRequest = paths["/api/dev/pets"]["post"]["requestBody"]["content"]["application/json"];
+
+export interface myPetPostType {
+  breedId: number | null;
+  name: string;
+  gender: "F" | "M" | null;
+  age: number | null;
+  diseaseIds: number[] | null;
+  symptomIds: number[];
+}
+
+export const postMyPet = async (data: myPetPostType) => {
+  return await post<myPetPostType>(`${API_PATH.PETS}`, data);
+};
