dca5ad892c431f543d79da7b963833b94fa68560
yarimu <yhli0320@naver.com>
2025-01-22 14:40:01 +0900

    fix: 닉네임 없는 경우 반영

--- src/page/onboarding/complete/Complete.tsx
+++ src/page/onboarding/complete/Complete.tsx
@@ -19,13 +19,14 @@ const Complete = () => {
 
   // api
   const { data: nickname } = useGetNickname();
+  if (!nickname) return;
 
   return (
     <div className={styles.backGround}>
       <div className={styles.layout}>
         <div className={styles.titleWrapper}>
           <div>
-            <Title text={`${nickname?.nickname}님`} />
+            <Title text={`${nickname.nickname}님`} />
             <Title text={"반려동물을 등록해 주세요"} />
           </div>
           <Docs text={"작은 정보가 우리 아이의 건강을 지켜요"} />
