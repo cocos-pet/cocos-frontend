bbd7a84132d0088494c9e1dec10354d260956169
kong <piorag99@gmail.com>
2025-01-24 06:25:07 +0900

    fix: 검색 결과 필터 제대로 동작하도록 수정, id 형식에 맞게 뜨도록 수정

--- src/page/community/search/done/SearchDone.tsx
+++ src/page/community/search/done/SearchDone.tsx
@@ -8,7 +8,7 @@ import Content from "@common/component/Content/Content.tsx";
 import { usePostPostFilters } from "@api/domain/community/search/hook.ts";
 import { useFilterStore } from "@store/filter.ts";
 import FilterBottomSheet from "@shared/component/FilterBottomSheet/FilterBottomSheet.tsx";
-import { useGetBodies, useGetDisease, useGetSymptoms } from "@api/domain/mypage/edit-pet/hook.ts";
+import { useGetAnimal, useGetBodies, useGetDisease, useGetSymptoms } from "@api/domain/mypage/edit-pet/hook.ts";
 import { formatTime } from "@shared/util/formatTime";
 import noSearchResult from "@asset/image/noSearchResult.png";
 
@@ -41,15 +41,19 @@ const SearchDone = () => {
   const { data: symptomBodies } = useGetBodies("SYMPTOM");
   const { data: symptoms } = useGetSymptoms(bodyDiseaseIds);
   const { data: disease } = useGetDisease(bodySymptomsIds);
+  const { data: animal } = useGetAnimal();
 
   useEffect(() => {
+    if (animal?.animals) {
+      setCategoryData("breeds", animal.animals);
+    }
     if (symptoms?.bodies) {
       setCategoryData("symptoms", symptoms.bodies);
     }
     if (disease?.bodies) {
       setCategoryData("disease", disease.bodies);
     }
-  }, [symptoms, disease]);
+  }, [symptoms, disease, animal]);
 
   useEffect(() => {
     if (diseaseBodies?.bodies && symptomBodies?.bodies) {
@@ -111,34 +115,8 @@ const SearchDone = () => {
   };
 
   const onClickPost = (postId: number | undefined) => {
-    navigate(`${PATH.COMMUNITY.ROOT}/${postId}}`);
+    navigate(`${PATH.COMMUNITY.ROOT}/${postId}`);
   };
-
-  if (searchDoneData.length === 0)
-    return (
-      <div className={styles.container}>
-        <div className={styles.searchHeader}>
-          <IcLeftarrow className={styles.icon} onClick={onBackClick} />
-          <TextField
-            value={searchText}
-            placeholder={"검색어를 입력해주세요"}
-            onChange={onChange}
-            icon={<IcSearch />}
-            onClearClick={() => setSearchText("")}
-            onClick={onTextFieldClick}
-          />
-        </div>
-        <div className={styles.noSearchData}>
-          <img className={styles.noSearchResultImage} src={noSearchResult} alt="검색 결과 없음" />
-          <span className={styles.noSearchText}>검색 결과를 찾지 못했어요.</span>
-          <span className={styles.noSearchRecommendText}>
-            {"검색어를 확인하거나"}
-            <br />
-            {"다른 키워드로 검색해 보세요."}
-          </span>
-        </div>
-      </div>
-    );
 
   return (
     <div className={styles.container}>
@@ -168,20 +146,34 @@ const SearchDone = () => {
             }}
           />
         )}
-        {searchDoneData?.map((data, index) => (
-          <Content
-            key={`search-done-${data.id}`}
-            breed={data?.breed}
-            petAge={data?.petAge}
-            postTitle={data?.title}
-            postContent={data.content}
-            likeCnt={data.likeCount}
-            commentCnt={data.commentCount}
-            timeAgo={formatTime(data.createdAt as string)}
-            onClick={() => onClickPost(data?.id)} //TODO: postId 로 변경
-          />
-        ))}
+
+        {searchDoneData.length === 0 ? (
+          <div className={styles.noSearchData}>
+            <img className={styles.noSearchResultImage} src={noSearchResult} alt="검색 결과 없음" />
+            <span className={styles.noSearchText}>검색 결과를 찾지 못했어요.</span>
+            <span className={styles.noSearchRecommendText}>
+              {"검색어를 확인하거나"}
+              <br />
+              {"다른 키워드로 검색해 보세요."}
+            </span>
+          </div>
+        ) : (
+          searchDoneData?.map((data) => (
+            <Content
+              key={`search-done-${data.id}`}
+              breed={data?.breed}
+              petAge={data?.petAge}
+              postTitle={data?.title}
+              postContent={data.content}
+              likeCnt={data.likeCount}
+              commentCnt={data.commentCount}
+              timeAgo={formatTime(data.createdAt as string)}
+              onClick={() => onClickPost(data.id)}
+            />
+          ))
+        )}
       </div>
+
       <FilterBottomSheet />
     </div>
   );
