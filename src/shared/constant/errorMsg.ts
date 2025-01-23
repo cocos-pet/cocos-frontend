768bf8a14012a3be25b6db2aa4e5a513f4e85386
yarimu <yhli0320@naver.com>
2025-01-24 04:17:14 +0900

    fix: 펫이름 길이제한 변경

--- src/shared/constant/errorMsg.ts
+++ src/shared/constant/errorMsg.ts
@@ -8,7 +8,7 @@ export const ERROR_MSG = {
 
   petName: {
     enNum: "숫자나 영어는 사용할 수 없어요",
-    length: "2자 이상 8자 이하로 입력해주세요",
+    length: "8자 이하로 입력해주세요",
     spaceSymbol: "공백이나 특수문자는 사용할 수 없어요",
     ko: "자음, 모음은 단독으로 사용할 수 없어요",
   },
