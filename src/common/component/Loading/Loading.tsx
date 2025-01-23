41a40d5b1a780a1309d14a0fd242892cc2a916ce
yarimu <yhli0320@naver.com>
2025-01-23 21:45:35 +0900

    feat: 로딩 로띠

--- src/common/component/Loading/Loading.tsx
+++ src/common/component/Loading/Loading.tsx
@@ -1,10 +1,11 @@
 import { Player } from "@lottiefiles/react-lottie-player";
 import loading from "@asset/lottie/loading.json";
+import { layout } from "./Loading.css";
 
 const Loading = () => {
   return (
-    <div>
-      <Player autoplay loop={false} src={loading} />
+    <div className={layout}>
+      <Player autoplay loop src={loading} style={{ width: 127, height: 129 }} />
     </div>
   );
 };
