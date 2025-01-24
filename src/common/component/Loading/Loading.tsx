134118b400c10494b204dba2a96852e961c9d936
yarimu <yhli0320@naver.com>
2025-01-24 11:23:48 +0900

    feat: 로딩 컴포넌트 라이브러리 변경 버전 반영

--- src/common/component/Loading/Loading.tsx
+++ src/common/component/Loading/Loading.tsx
@@ -1,11 +1,12 @@
-import { Player } from "@lottiefiles/react-lottie-player";
 import loading from "@asset/lottie/loading.json";
 import { layout } from "./Loading.css";
+import Lottie from "lottie-react";
 
 const Loading = () => {
   return (
     <div className={layout}>
-      <Player autoplay loop src={loading} style={{ width: 127, height: 129 }} />
+      {/* Lottie 애니메이션 */}
+      <Lottie animationData={loading} autoplay={true} style={{ width: 127, height: 129 }} />
     </div>
   );
 };
