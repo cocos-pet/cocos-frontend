5509e7ade0dd09a553066f2712508a3b38b303cc
minjeoong <orange_mj@naver.com>
2025-03-14 22:10:53 +0900

    feat: 글로벌 스타일 Next.js 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./route/Router";
 //import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./style/global.css.ts";
 
 function App() {
   const queryClient = new QueryClient({
