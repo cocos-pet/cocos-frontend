5b32d5c35bb2146c0aa55311ad53e865cb19c3a2
kong <piorag99@gmail.com>
2025-04-05 20:34:58 +0900

    test

---
+++ .next/static/chunks/pages/_app-77ab4fe514fade9c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{3756:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(7770)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(3756),n(9732))),_N_E=_.O()}]);
