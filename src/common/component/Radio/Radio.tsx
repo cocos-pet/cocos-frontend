8fdaee45af04304612d019b22aa99e0ff2ec2576
yarimu <yhli0320@naver.com>
2025-04-09 02:39:49 +0900

    feat: 공통 Radio 컴포넌트 기본 UI 구현

---
+++ src/common/component/Radio/Radio.tsx
@@ -0,0 +1,15 @@
+import * as styles from "./Radio.css";
+
+const Radio = () => {
+  return (
+    <input
+      type="radio"
+      className={styles.radio}
+      // value={value}
+      // checked={isChecked}
+      // onChange={() => onChange(value)}
+    />
+  );
+};
+
+export default Radio;
