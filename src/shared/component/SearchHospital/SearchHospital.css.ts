3f8036e36457b17042178fd0d23a7611b3139b5f
yarimu <yhli0320@naver.com>
2025-04-09 17:29:38 +0900

    feat: 병원선택 뷰 구현

--- src/shared/component/SearchHospital/SearchHospital.css.ts
+++ src/shared/component/SearchHospital/SearchHospital.css.ts
@@ -1,42 +1,61 @@
 import { style } from "@vanilla-extract/css";
-import {color} from "@style/styles.css";
+import { color } from "@style/styles.css";
 
+export const Wrapper = style({
+  position: "relative",
+  marginTop: "8rem",
 
-export const Wrapper =  style({
-        position:"relative",
-        marginTop:"8rem",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width:"100%",
-        height: "calc(100vh - 8rem)",
-        borderRadius: "20px 20px 0px 0px",
-        backgroundColor:"gray",
-        // backgroundColor:color.gray.gray000,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
 
-    });
+  width: "100%",
+  height: "calc(100vh - 8rem)",
+  overflow: "hidden",
 
-export const bottomSheetHandleBar = style({
-    position:"absolute",
-    top:"1rem",
-    width:"8rem",
+  borderRadius: "20px 20px 0px 0px",
+  // 임시색상
+  backgroundColor: "gray",
+  // backgroundColor:color.gray.gray000,
 });
 
-export const textFieldStyle = style({
-    position:"absolute",
-    top:"2.4rem",
-    width:"100%",    
-    padding:"1.2rem 2rem",
-})
+export const bottomSheetHandleBar = style({
+  position: "absolute",
+  top: "1rem",
+  width: "8rem",
+});
+
+export const textFieldContainer = style({
+  position: "absolute",
+  top: "2.4rem",
+  width: "100%",
+  padding: "1.2rem 2rem",
+});
+
+export const cardContainer = style({
+  position: "absolute",
+  top: "12rem",
+  width: "100%",
+  height: "calc(100vh - 33.2rem)",
+  overflow: "auto",
+});
+
+// 마지막 li 요소에 밑줄 추가
+export const lastCard = style({
+  borderBottom: `1px solid ${color.gray.gray300}`,
+});
 
 export const buttonContainer = style({
-    position:"absolute",
-    bottom:"3.2rem",
-    display:"flex",
-    flexDirection: "column",
-    gap:"0.8rem",
-    width:"100%",
-    padding:"0 2rem",
+  position: "absolute",
+  bottom: "0rem",
 
-})
+  display: "flex",
+  flexDirection: "column",
+  gap: "0.8rem",
+
+  width: "100%",
+  height: "12.6rem",
+  padding: "0 2rem 3.2rem 2rem",
+
+  backgroundColor: color.gray.gray000,
+});
--- src/shared/component/SearchHospital/SearchHospital.tsx
+++ src/shared/component/SearchHospital/SearchHospital.tsx
@@ -1,23 +1,55 @@
-import * as styles from "./SearchHospital.css"
+import * as styles from "./SearchHospital.css";
 import IcBottomSheetLine from "@asset/svg/IcBottomSheetLine";
 import { Button } from "@common/component/Button";
 import { TextField } from "@common/component/TextField";
+import Card from "./Card";
+import { useState } from "react";
+
+// ⚠️ 삭제 예정 목데이터
+const hospitals = [
+  { id: 1, name: "서울병원", address: "서울시강남구" },
+  { id: 2, name: "부산병원", address: "서울시강남구" },
+  { id: 3, name: "대구병원", address: "서울시강남구" },
+  { id: 4, name: "광주병원", address: "서울시강남구" },
+  { id: 5, name: "천안병원", address: "서울시강남구" },
+  { id: 6, name: "춘천병원", address: "서울시강남구" },
+];
 
 const SearchHospital = () => {
+  const [selectedHospitalId, setSelectedHospitalId] = useState<number>(0);
 
   return (
-    <div className={styles.Wrapper} >
-        <div className={styles.bottomSheetHandleBar}><IcBottomSheetLine/></div>
-        <div className={styles.textFieldStyle}>
-          <TextField value="hospitalName"/>
-        </div>
-        <div className={styles.buttonContainer}>
-          <Button label="확인하기" size="large" variant="solidPrimary" disabled={false} />
-          <Button label="취소하기" size="large" variant="solidNeutral" disabled={false}  />
-        </div>
-    </div>
-    
-  )
-}
+    <div className={styles.Wrapper}>
+      <div className={styles.bottomSheetHandleBar}>
+        <IcBottomSheetLine />
+      </div>
+      {/* 상단 검색창 영역 */}
+      <div className={styles.textFieldContainer}>
+        <TextField value="hospitalName" />
+      </div>
 
-export default SearchHospital;                     
+      {/* 중앙 병원 리스트 영역 */}
+      <ul className={styles.cardContainer}>
+        {hospitals.map((hospital) => (
+          <li key={hospital.id} className={hospital.id === hospitals.length ? styles.lastCard : undefined}>
+            <Card
+              id={hospital.id}
+              name={hospital.name}
+              address={hospital.address}
+              selected={hospital.id === selectedHospitalId}
+              onSelect={setSelectedHospitalId}
+            />
+          </li>
+        ))}
+      </ul>
+
+      {/* 하단 버튼 영역 */}
+      <div className={styles.buttonContainer}>
+        <Button label="확인하기" size="large" variant="solidPrimary" disabled={false} />
+        <Button label="취소하기" size="large" variant="solidNeutral" disabled={false} />
+      </div>
+    </div>
+  );
+};
+
+export default SearchHospital;
