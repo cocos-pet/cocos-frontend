b74333ace28d8cf58d20b16a4669ec245925efc0
Tavian <155794105+ocahs9@users.noreply.github.com>
2025-05-03 21:41:48 +0900

    Revert "Revert "[Feat/#255] 병원리뷰 (1/4)뷰 "" (#275)

--- src/asset/svg/IcChevronRight2.tsx
+++ src/asset/svg/IcChevronRight2.tsx
@@ -1,20 +1,20 @@
 import * as React from "react";
 import type { SVGProps } from "react";
 
-const SvgIcRightArror = (props: SVGProps<SVGSVGElement>) => {
+const SvgIcChevronRight2 = (props: SVGProps<SVGSVGElement>) => {
   const { stroke = "#717171", ...rest } = props;  // 기본 stroke 색상 설정
 
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" {...rest}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 21" {...rest}>
       <path
-        stroke={stroke} 
+        stroke={stroke}  // stroke 속성 적용
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth={1.5}
-        d="m7.5 15 5-5-5-5"
+        d="m5.023 8 5 5 5-5"
       />
     </svg>
   );
 };
 
-export default SvgIcRightArror;
+export default SvgIcChevronRight2;
--- src/common/component/TextField/styles.css.ts
+++ src/common/component/TextField/styles.css.ts
@@ -25,6 +25,7 @@ export const styles = {
           padding: "1.2rem",
         },
         centerPlaceholder: { maxWidth: "12rem" },
+
         search: {
           width: "100%",
           marginLeft: "2rem",
@@ -45,10 +46,17 @@ export const styles = {
           border: "none",
         },
       },
+      focus: {
+        true: {
+          border: `0.1rem solid ${color.primary.blue600}`,
+        },
+        false: {},
+      },
     },
     defaultVariants: {
       state: "default",
       active: true,
+      focus: false,
     },
   }),
 
