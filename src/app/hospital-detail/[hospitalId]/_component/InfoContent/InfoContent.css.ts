3ee032edc4e5ae5196d029f13cbbffe2d804d8f8
이윤지 <rhaxlddbswl3@gmail.com>
2025-05-08 03:58:33 +0900

    위치 제외 퍼블리싱 완료

--- src/app/hospital-detail/[hospitalId]/_component/InfoContent/InfoContent.css.ts
+++ src/app/hospital-detail/[hospitalId]/_component/InfoContent/InfoContent.css.ts
@@ -6,19 +6,19 @@ export const container = style({
   flexDirection: "column",
 });
 
-
 export const tags = style({
   display: "flex",
   gap: "0.4rem",
   flexWrap: "wrap",
-  padding:"1.2rem 2rem 1.2rem 1.6rem"
+  padding: "1.2rem 2rem 1.2rem 1.6rem",
 });
 
 export const tag = style([
-  font.body01,{
-    color:color.primary.blue700,
-    fontWeight:500,
-  }
+  font.body01,
+  {
+    color: color.primary.blue700,
+    fontWeight: 500,
+  },
 ]);
 
 export const noTag = style({
@@ -27,33 +27,32 @@ export const noTag = style({
 });
 
 export const introduction = style({
-  backgroundColor:"#FAFAFA",
-  display:"inline-flex",
-      alignItems:"center",
-      gap:"1rem",
+  backgroundColor: "#FAFAFA",
+  display: "inline-flex",
+  alignItems: "center",
+  gap: "1rem",
 });
 
 export const introductionText = style([
-    font.body01,
+  font.body01,
   {
-
-      padding:"0.8rem 2.8rem 0.8rem 2.8rem",
-      color:color.gray.gray700,
-  }
-  ]);
-  
+    padding: "0.8rem 2.8rem 0.8rem 2.8rem",
+    color: color.gray.gray700,
+  },
+]);
 
 export const addressRow = style({
   display: "flex",
   alignItems: "center",
-  padding:"2rem 2rem 0rem 2rem",
+  padding: "2rem 2rem 0rem 2rem",
   gap: "0.8rem",
 });
 
 export const address = style([
-    font.heading03,{
-        fontWeight:600,
-    }
+  font.heading03,
+  {
+    fontWeight: 600,
+  },
 ]);
 
 export const copyIcon = style({
@@ -63,23 +62,23 @@ export const copyIcon = style({
 });
 
 export const mapWrapper = style({
-  padding:"1.6rem 2rem 10.445rem 2rem ",
+  padding: "1.6rem 2rem 10.445rem 2rem ",
 });
 
 export const toast = style([
   font.label01,
   {
-      color: color.gray.gray000,
-      display: "flex",
-      alignItems: "center",
-      height: "4rem",
-      backgroundColor: color.primary.blue500,
-      padding: "0.8rem 1.6rem",
-      borderRadius: "0.8rem",
-      position: "fixed",
-      left: "2rem",
-      right: "2rem",
-      bottom: "2.4rem",
-      zIndex: 9999,
-  }
-])
+    color: color.gray.gray000,
+    display: "flex",
+    alignItems: "center",
+    height: "4rem",
+    backgroundColor: color.primary.blue500,
+    padding: "0.8rem 1.6rem",
+    borderRadius: "0.8rem",
+    position: "fixed",
+    left: "2rem",
+    right: "2rem",
+    bottom: "2.4rem",
+    zIndex: 9999,
+  },
+]);
