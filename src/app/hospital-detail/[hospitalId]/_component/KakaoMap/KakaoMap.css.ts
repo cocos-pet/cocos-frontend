124733d271103ab633157b304babc44f2dea12cb
이윤지 <rhaxlddbswl3@gmail.com>
2025-05-10 02:00:55 +0900

    feat: 위치 스타일 수정2

--- src/app/hospital-detail/[hospitalId]/_component/KakaoMap/KakaoMap.css.ts
+++ src/app/hospital-detail/[hospitalId]/_component/KakaoMap/KakaoMap.css.ts
@@ -1,8 +1,22 @@
-import { style } from '@vanilla-extract/css';
+import { style } from "@vanilla-extract/css";
+import { font } from "@style/styles.css";
 
-export const mapContainer = style({
-  width: '100%',
-  height: '400px',
-  borderRadius: '8px',
-  overflow: 'hidden'
-}); 
+export const address = style([
+    font.heading03,
+    {
+        fontWeight:600,
+        position:"absolute",
+        display:"flex",
+        flexDirection:"column",
+        height:"7rem",
+        width:"100%",
+        zIndex:2,
+        backgroundColor:"white",
+        marginTop:"9.1rem",
+        padding:"1.2rem 1.2rem",
+        borderRadius:"1rem",
+        borderRight: "1px solid #E4E4E4",
+        borderBottom: "1px solid #E4E4E4",
+        borderLeft: "1px solid #E4E4E4",
+    }
+]);
