4a5a8d407a70291586d5dfc0bac9ab897d333575
yarimu <yhli0320@naver.com>
2025-05-10 00:26:31 +0900

    feat: Step1 섹션화

--- src/app/review/write/page.tsx
+++ src/app/review/write/page.tsx
@@ -1,72 +1,48 @@
 "use client";
 
-import { useState } from "react";
-import { IcDeleteBlack } from "@asset/svg/index";
-import * as styles from "./styles.css";
+import { FormProvider, useForm } from "react-hook-form";
 
-import HeaderNav from "@common/component/HeaderNav/HeaderNav";
-import ReviewHospital from "@app/review/write/_component/ReviewHospital";
-import ReviewDate from "@app/review/write/_component/ReviewDate";
-import ReviewPetInfo from "@app/review/write/_component/ReviewPetInfo";
-import SearchHospital, { Hospital } from "@shared/component/SearchHospital/SearchHospital";
-import { Button } from "@common/component/Button/index";
+// import Step1 from "@app/review/write/_section/Step1";
+// import Step2 from "@app/review/write/_section/Step2";
+import Step3 from "@app/review/write/_section/Step3";
+// 제출할 리뷰 데이터
+export interface ReviewFormData {
+  hospital: string; // ⚠️ 이거 필요없음 다른 pr 머지되면 수정 예정
+  date: string; // ⚠️ 키 visitedAt으로 바꿔야함
+  symptomIds: number[];
+  diseaseId: number;
+  purposeId: number;
+  // breedId:number;
+  // gender: string;
+  // weight: number;
+  // visitedAt: string
+  // content: string;
+  // diseaseId: number;
+  // goodReviewIds: number[];
+  // badReviewIds: number[];
+  // images: string[];
+}
 
-export type PetInfoType = "myPet" | "manual";
-
-const Page = () => {
-  // 병원 검색 바텀시트 열고 닫기
-  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
-  // 선택된 병원
-  const [selectedHospital, setSelectedHospital] = useState<Hospital | null>(null);
-  // 동물 정보 입력 방법 선택
-  const [selectedPetInfo, setSelectedPetInfo] = useState<PetInfoType | null>(null);
-
-  // 1-1. hospital ⚠️ 나갈 수 있는 방법이 2가지라 분리
-  const handleOpenSearchHospital = () => {
-    setIsBottomSheetOpen(true);
-  };
-  const handleCloseBottomSheet = () => {
-    setIsBottomSheetOpen(false);
-  };
-
-  const handleSelectHospital = (hospital: Hospital | null) => {
-    setSelectedHospital(hospital);
-  };
-
-  // 1-3. petInfo
-  const handleSelectPetInfo = (type: PetInfoType) => {
-    setSelectedPetInfo((prev) => (prev === type ? null : type));
-  };
-
-  return (
-    <div className={styles.preventScroll}>
-      {/* 상단 헤더 */}
-      <HeaderNav centerContent="리뷰작성(1/4)" leftIcon={<IcDeleteBlack style={{ width: 24, height: 24 }} />} />
-
-      {/* 중앙 컨텐츠 */}
-      <div className={styles.wrapper}>
-        {/* 1-1. 병원 검색 */}
-        <ReviewHospital selectedHospital={selectedHospital} handleOpenSearchHospital={handleOpenSearchHospital} />
-        {/* 1-2. 날짜 선택 */}
-        <ReviewDate />
-        {/* 1-3. 동물 정보 */}
-        <ReviewPetInfo selectedPetInfo={selectedPetInfo} onSelectPetInfo={handleSelectPetInfo} />
-      </div>
-
-      {/* 하단 버튼 */}
-      <div className={styles.buttonContainer}>
-        <Button label="다음으로" size="large" variant="solidPrimary" disabled={true} />
-      </div>
-
-      {/* 병원 검색 바텀시트 */}
-      <SearchHospital
-        active={isBottomSheetOpen}
-        onCloseBottomSheet={handleCloseBottomSheet}
-        selectedHospital={selectedHospital}
-        onSelectHospital={handleSelectHospital}
-      />
-    </div>
-  );
+const defaultValues: ReviewFormData = {
+  hospital: "",
+  date: "",
+  symptomIds: [],
+  diseaseId: -1,
+  purposeId: -1,
 };
 
-export default Page;
+const page = () => {
+  const methods = useForm<ReviewFormData>({
+    defaultValues,
+    mode: "onChange",
+  });
+
+  return (
+    <FormProvider {...methods}>
+      {/* <Step1 /> */}
+      {/* <Step2 /> */}
+      <Step3 />
+    </FormProvider>
+  );
+};
+export default page;
