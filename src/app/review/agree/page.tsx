bed1f8b92c9dc2cf027314a73b21ffd3318047ee
yarimu <yhli0320@naver.com>
2025-05-10 23:36:04 +0900

    feat: 누락 이미지 첨부

--- src/app/review/agree/page.tsx
+++ src/app/review/agree/page.tsx
@@ -7,7 +7,7 @@ import { TITLE, CHECKBOX_TEXTS } from "@app/review/agree/constant";
 import Image from "next/image";
 
 import { IcLeftarrow } from "@asset/svg";
-import reviewNoticeFrame from "@asset/image/reviewNoticeFrame.png";
+import reviewAgreeImg from "@asset/image/review-agree.png";
 import danger from "@asset/image/danger.png";
 import { IcCheckbox } from "@asset/svg";
 import { useRouter } from "next/navigation";
@@ -67,7 +67,7 @@ const page = () => {
           <p className={style.docs}>{TITLE.descriptions[1]}</p>
         </section>
 
-        <Image src={reviewNoticeFrame} alt="리뷰작성 유의사항 이미지" priority className={style.mainImg} />
+        <Image src={reviewAgreeImg} alt="리뷰작성 유의사항 이미지" priority className={style.mainImg} />
 
         <section className={style.bottomLayout}>
           {CHECKBOX_TEXTS.map((text, idx) => (
