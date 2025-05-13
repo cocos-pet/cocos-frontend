764697ab0357db08c78b052e0abdba9a983e3150
이윤지 <rhaxlddbswl3@gmail.com>
2025-05-14 03:16:31 +0900

    refactor: info 스타일 수정

--- src/app/hospital-detail/[hospitalId]/_component/Info/Info.css.ts
+++ src/app/hospital-detail/[hospitalId]/_component/Info/Info.css.ts
@@ -3,18 +3,17 @@ import { color, font } from "@style/styles.css";
 
 export const infoContainer = style({
     display: "flex",
-    padding: "3.2rem 0rem",
+    padding: "3.2rem 0rem 4rem 0rem",
     flexDirection: "column",
     alignItems:"center",
     gap:"1.6rem",
-    alignSelf:"stretch"
+    alignSelf:"stretch",
 });
 
 export const infoName = style([
     font.heading01,
     {
         fontWeight: 600,
-        marginTop:"3.2rem",
     }
 ]);
 
@@ -26,7 +25,6 @@ export const infoPhoneNumber = style([
         gap: "0.8rem",
         fontWeight: 500,
         color: color.gray.gray700,
-        marginTop: "1.6rem"
     }
 ]);
 
