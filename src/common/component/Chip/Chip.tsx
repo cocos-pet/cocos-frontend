7d96e565dda9d585b47203a4476f3ddb2228bc6d
minjeoong <orange_mj@naver.com>
2025-05-22 00:28:34 +0900

    feat: Chip 로직 수정
    Signed-off-by: minjeoong <orange_mj@naver.com>

--- src/common/component/Chip/Chip.tsx
+++ src/common/component/Chip/Chip.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { chipItem, ChipType } from "./ChipStyle.css.ts";
-import { IcDelete } from "@asset/svg/index";
+import React, {useEffect, useState} from "react";
+import {chipItem, ChipType} from "./ChipStyle.css.ts";
+import {IcDelete} from "@asset/svg/index";
 
 interface ChipProps {
   label?: string;
@@ -41,25 +41,17 @@ const Chip = ({
   return (
     <div className={chipItem({ size, color, active: isActive })} onClick={handleClick}>
       {label}
-      {icon && (
-        <IcDelete
-          width={24}
-          height={24}
-          stroke={color === "gray" ? "#717171" : "#14B5F0"}
-          style={{
-            position: "relative",
-            bottom: "1.3px",
-          }}
-        />
-      )}
-      {rightIcon && (
-        <div
-          style={{
-            marginLeft: "4px",
-          }}
-        >
-          {rightIcon}
-        </div>
+      {rightIcon ? (
+        <>{rightIcon}</>
+      ) : (
+        icon && (
+          <IcDelete
+            width={24}
+            height={24}
+            stroke={color === "gray" ? "#717171" : "#14B5F0"}
+            style={{ position: "relative", bottom: "1.3px" }}
+          />
+        )
       )}
     </div>
   );
