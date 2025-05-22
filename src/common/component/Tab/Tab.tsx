16c096a572ac53f244121e097d063584e07ce8ce
yarimu <yhli0320@naver.com>
2025-05-23 04:01:03 +0900

    feat: 공통 부분 (리뷰3페이지~4페이지 PR)

--- src/common/component/Tab/Tab.tsx
+++ src/common/component/Tab/Tab.tsx
@@ -4,14 +4,15 @@ interface TabPropTypes {
   children: string;
   width?: string;
   active?: boolean;
+  variant?: "good" | "bad";
   onClick?: () => void;
 }
 
-const Tab = ({ children, width, onClick, active = false }: TabPropTypes) => {
+const Tab = ({ children, width, onClick, active = false, variant = "good" }: TabPropTypes) => {
   return (
     <div style={{ width: width }} className={styles.tab} onClick={onClick}>
       <div className={styles.tabContent}>{children}</div>
-      <div className={styles.tabBar({ active: active })} />
+      <div className={styles.tabBar({ active: active, variant })} />
     </div>
   );
 };
