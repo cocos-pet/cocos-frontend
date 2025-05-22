fcfbdd3bf111828c833394cf5132efa909107910
yarimu <yhli0320@naver.com>
2025-05-23 04:01:30 +0900

    feat: 개인 부분 (리뷰3페이지~4페이지 PR)

---
+++ src/app/review/write/_component/ReviewContent.tsx
@@ -0,0 +1,26 @@
+import { useFormContext } from "react-hook-form";
+import { ReviewFormData } from "@app/review/write/page";
+import TextArea from "@app/community/_component/TextArea/TextArea";
+import * as styles from "./ReviewContent.style.css";
+
+const ReviewContent = () => {
+  const { watch, setValue } = useFormContext<ReviewFormData>();
+  const contentValue = watch("content");
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue("content", e.target.value);
+  };
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.title}>상세 후기</h2>
+      <TextArea
+        value={contentValue}
+        onChange={handleChange}
+        placeholder={
+          "진료 경험에 대한 상세 후기를 작성해 주세요.\n동일한 증상을 겪는 반려인들에게 큰 도움이 될 거예요!"
+        }
+      />
+    </div>
+  );
+};
+
+export default ReviewContent;
