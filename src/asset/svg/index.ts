955ed8d97cf8c006d904052475a5d76c6a6f0b00
yarimu <yhli0320@naver.com>
2025-05-23 04:29:02 +0900

    feat: 공통 부분 (캘린더 커스텀)

--- src/asset/svg/index.ts
+++ src/asset/svg/index.ts
@@ -12,6 +12,8 @@ export { default as IcRightIcon } from "./IcRightIcon";
 export { default as IcShape } from "./IcShape";
 export { default as IcAddphoto } from "./IcAddphoto";
 export { default as IcBaseProfileImage } from "./IcBaseProfileImage";
+export { default as IcCalenderLeft } from "./IcCalenderLeft";
+export { default as IcCalenderRight } from "./IcCalenderRight";
 export { default as IcChevronLeft } from "./IcChevronLeft";
 export { default as IcChevronRight } from "./IcChevronRight";
 export { default as IcChevronRight2 } from "./IcChevronRight2";
