afcf7ba1169699f99dbbac4e9c59832dba1b6bef
이윤지 <rhaxlddbswl3@gmail.com>
2025-05-26 01:49:49 +0900

    fix: hospital-detal -> hospitals api 이름 변경

--- src/api/constants/apiPath.ts
+++ src/api/constants/apiPath.ts
@@ -22,7 +22,7 @@ export const API_PATH = {
   POST_POPULAR: "/api/dev/posts/popular",
   POST_CATEGORIES: "/api/dev/posts/categories",
 
-  HOSPITAL_DETAIL: "/api/dev/hospitals",
+  HOSPITALS: "/api/dev/hospitals",
 
   TEST_HEALTH_CHECK: "/api/dev/test/health-check",
   TEST_TOKEN_CHECK: "/api/dev/test/token-check",
