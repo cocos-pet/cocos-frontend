da6e6a458d97b2180ac50badf822bdab96d3584d
minjeoong <orange_mj@naver.com>
2025-06-11 19:39:13 +0900

    feat: NoData 와 로딩 처리
    Signed-off-by: minjeoong <orange_mj@naver.com>

--- src/app/main/_section/hotPost/HotPost.tsx
+++ src/app/main/_section/hotPost/HotPost.tsx
@@ -6,30 +6,20 @@ import { useQueryGetPopular } from "@api/domain/main/hook.ts";
 import { PATH } from "@route/path.ts";
 import { useRouter } from "next/navigation";
 import { Separated } from "react-simplikit";
+import NoData from "@shared/component/NoData/NoData.tsx";
+import Loading from "@common/component/Loading/Loading.tsx";
 
-// interface Post {
-//   id?: number;
-//   title?: string;
-// }
-
-interface HotPostProps {
-  nickname?: string;
-}
-
-const HotPost = ({ nickname }: HotPostProps) => {
+const HotPost = () => {
   const router = useRouter();
 
   const { data: postsData, isLoading, isError } = useQueryGetPopular();
 
   if (isLoading) {
-    return <p>로딩 중...</p>;
+    return <Loading height={30} />;
   }
 
-  if (isError || !postsData?.data?.posts) {
-    return <p>데이터를 불러오는 데 실패했습니다.</p>;
-  }
+  const posts = postsData?.data?.posts || [];
 
-  const posts = postsData.data.posts || [];
   const handlePostClick = (postId?: number) => {
     if (postId !== undefined) {
       router.push(`${PATH.COMMUNITY.ROOT}/${postId}`);
@@ -42,18 +32,22 @@ const HotPost = ({ nickname }: HotPostProps) => {
         <p className={styles.subTitle}>인기 게시물을 확인해보세요</p>
         <div className={styles.title}>반려인들이 주목하는 글 TOP 5</div>
       </div>
-      <div className={styles.hotPostListContainer}>
-        <Separated by={<Divider size="popular" />}>
-          {posts.slice(0, 5).map((post, index) => (
-            <div key={post.id} className={styles.postlist}>
-              <div className={styles.postContent} onClick={() => handlePostClick(post.id)}>
-                <div className={styles.contentId}>{index + 1}</div>
-                <div className={styles.contentTitle}>{post.title}</div>
+      {!posts || isError ? (
+        <NoData style={{ marginTop: "10px" }} />
+      ) : (
+        <div className={styles.hotPostListContainer}>
+          <Separated by={<Divider size="popular" />}>
+            {posts.slice(0, 5).map((post, index) => (
+              <div key={post.id} className={styles.postlist}>
+                <div className={styles.postContent} onClick={() => handlePostClick(post.id)}>
+                  <div className={styles.contentId}>{index + 1}</div>
+                  <div className={styles.contentTitle}>{post.title}</div>
+                </div>
               </div>
-            </div>
-          ))}
-        </Separated>
-      </div>
+            ))}
+          </Separated>
+        </div>
+      )}
     </div>
   );
 };
