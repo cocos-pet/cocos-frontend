439af3a29441119c637bdf772cb1a5b7e923cf9c
yarimu <yhli0320@naver.com>
2025-06-29 16:32:30 +0900

    design: 병원정보 card컴포넌트 레이아웃 수정

--- src/shared/component/SearchHospital/Card.tsx
+++ src/shared/component/SearchHospital/Card.tsx
@@ -1,7 +1,6 @@
 // 병원 정보 카드
 import * as styles from "./Card.css";
 import Image from "next/image";
-// ⚠️ 추후 삭제할 이미지
 import nicknameCoco from "@asset/image/nicknameCoco.png";
 import Radio from "@common/component/Radio/Radio";
 
@@ -17,10 +16,12 @@ type CardProps = {
 const Card = ({ id, name, address, selected, imgSrc, onSelect }: CardProps) => {
   return (
     <div className={styles.wrapper({ selected })}>
-      <Radio value={id} checked={selected} onChange={onSelect} />
-      <div className={styles.infoLayout}>
-        <span className={styles.name}>{name}</span>
-        <span className={styles.address}>{address}</span>
+      <div className={styles.box}>
+        <Radio value={id} checked={selected} onChange={onSelect} />
+        <div className={styles.infoLayout}>
+          <span className={styles.name}>{name}</span>
+          <span className={styles.address}>{address}</span>
+        </div>
       </div>
       <Image src={imgSrc ?? nicknameCoco} alt="hospital-exterior" className={styles.img} />
     </div>
