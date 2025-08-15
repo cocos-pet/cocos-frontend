cef590e1d4de1fe8197e8ea9ce85d2724b333e1d
이윤지 <rhaxlddbswl3@gmail.com>
2025-08-15 23:31:22 +0900

    feat: 메인 화면 캐러셀 비율 조정...

--- src/app/main/Main.css.ts
+++ src/app/main/Main.css.ts
@@ -1,4 +1,4 @@
-import {style} from "@vanilla-extract/css";
+import { style } from "@vanilla-extract/css";
 
 export const mainContainer = style({
   width: "100%",
@@ -8,8 +8,20 @@ export const mainContainer = style({
 
 export const headerContainer = style({
   position: "absolute",
-  top: "18rem",
-  padding: "0rem 2rem 0rem 2rem",
-  zIndex: 4,
+  top: "14.5rem",
+  padding: "0rem 2rem",
+  zIndex: 1000,
   width: "100%",
+  left: "50%",
+  transform: "translateX(-50%)",
+  "@media": {
+    "(min-width: 450px) and (max-width: 600px)": {
+      top: "24rem",
+      transform: "translate(-50%, -50%)",
+    },
+    "(min-width: 600px)": {
+      top: "34rem",
+      transform: "translate(-50%, -50%)",
+    },
+  },
 });
