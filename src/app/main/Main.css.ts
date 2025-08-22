0959d89f7a360ecb06313876b8120f6f69cd6ebb
이윤지 <rhaxlddbswl3@gmail.com>
2025-08-22 20:42:41 +0900

    fix: 메인화면 캐러셀 스타일 수정

--- src/app/main/Main.css.ts
+++ src/app/main/Main.css.ts
@@ -7,21 +7,8 @@ export const mainContainer = style({
 });
 
 export const headerContainer = style({
-  position: "absolute",
-  top: "14.5rem",
   padding: "0rem 2rem",
   zIndex: 1000,
   width: "100%",
-  left: "50%",
-  transform: "translateX(-50%)",
-  "@media": {
-    "(min-width: 450px) and (max-width: 600px)": {
-      top: "24rem",
-      transform: "translate(-50%, -50%)",
-    },
-    "(min-width: 600px)": {
-      top: "34rem",
-      transform: "translate(-50%, -50%)",
-    },
-  },
+  transform: "translateY(-50%)",
 });
