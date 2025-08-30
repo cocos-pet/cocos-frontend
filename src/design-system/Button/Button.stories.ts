aee007ee56c9c9a74679255f2ee99bbc302f4fee
minjeoong <orange_mj@naver.com>
2025-08-31 00:28:42 +0900

    feat: 스토리북 에러 해결

--- src/design-system/Button/Button.stories.ts
+++ src/design-system/Button/Button.stories.ts
@@ -1,5 +1,5 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { Button } from "./index";
+import type {Meta, StoryObj} from "@storybook/react";
+import {Button} from "./index";
 
 const meta: Meta<typeof Button> = {
   title: "Design System/Button",
