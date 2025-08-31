e678f0393e35bbe33d79a477d191f87aa24c39b2
Tavian <155794105+ocahs9@users.noreply.github.com>
2025-08-31 19:34:54 +0900

    [Feat/#447]센트리 설정 (#453)
    * feat: sentry/next 기본 설정
    
    * feat: env 추가
    
    * feat: masking off
    
    * feat: add release version, authToken for sourceMap
    
    * feat: mcp 추가
    
    * feat: 센트리 에러 테스트 페이지 추가
    
    * fix: client 모니터링 안되던 문제 수정(NEXT_PUBLIC_SENTRY_DSN)
    
    * fix: 잘못된 위계의 폴더 삭제 및 파일 이동
    
    * feat: 테스트용 클라이언트 에러페이지 추가

--- src/api/domain/review/write/feedback/hook.ts
+++ src/api/domain/review/write/feedback/hook.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getFeedback } from "@app/api/review/write/feedback";
+import { getFeedback } from "@api/domain/review/write/feedback";
 
 export const useFeedbackGet = () => {
   return useQuery({
