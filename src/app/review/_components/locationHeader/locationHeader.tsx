1d11d038115e13a848edb19a270d7e7c54a0b4d0
이윤지 <rhaxlddbswl3@gmail.com>
2025-09-03 23:16:25 +1000

    feat: 새로고침시에도 사용자 위치 유지

--- src/app/review/_components/locationHeader/locationHeader.tsx
+++ src/app/review/_components/locationHeader/locationHeader.tsx
@@ -8,6 +8,8 @@ import { updateMemberLocation } from "@api/domain/review/location";
 import { LocationType } from "@api/domain/review/location/types";
 import { DEFAULT_LOCATION } from "@app/review/_constant/locationConfig";
 
+const STORAGE_KEY = "selectedLocation";
+
 interface Location {
   id: number;
   name: string;
@@ -25,12 +27,21 @@ export default function LocationHeader({ onLocationChange, onBottomSheetOpenChan
   const { data: memberLocation } = useGetMemberLocation();
 
   useEffect(() => {
-    if (memberLocation?.locationId) {
-      setSelectedLocation({
+    // 로컬 스토리지에서 저장된 위치 정보 가져오기
+    const savedLocation = localStorage.getItem(STORAGE_KEY);
+    const parsedLocation = savedLocation ? JSON.parse(savedLocation) : null;
+
+    if (parsedLocation) {
+      setSelectedLocation(parsedLocation);
+      onLocationChange(parsedLocation);
+    } else if (memberLocation?.locationId) {
+      const newLocation = {
         id: memberLocation.locationId,
         name: memberLocation.locationName,
         type: "DISTRICT",
-      });
+      };
+      setSelectedLocation(newLocation as Location);
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(newLocation));
     }
   }, [memberLocation]);
 
@@ -58,6 +69,8 @@ export default function LocationHeader({ onLocationChange, onBottomSheetOpenChan
     setSelectedLocation(updatedLocation);
     onLocationChange(updatedLocation);
     handleBottomSheetClose();
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(updatedLocation));
 
     updateMemberLocation({
       locationId: location.id,
