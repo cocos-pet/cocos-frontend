b47ad0e26368676e4e0b2d11416ec0d04c09f9e6
Tavian <155794105+ocahs9@users.noreply.github.com>
2025-11-05 01:54:56 +0900

    [Feat/#480] 리뷰 페이지 프로필 클릭 시 동작 변경 (#481)
    * refactor: 로그인 모달 공통 컴포넌트화
    
    * chore: 상수명 변경
    
    * feat: 로그인 유무에 따른 프로필 클릭 동작 분기 처리

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -20,6 +20,7 @@ import { useIsPetRegistered } from "@common/hook/useIsPetRegistered";
 import { Modal } from "@common/component/Modal/Modal.tsx";
 import { useGetReviewAgreementStatus } from "@api/domain/review/agree/hook";
 import LazyImage from "@common/component/LazyImage";
+import LoginModal from "@common/component/LoginModal/LoginModal";
 
 interface Location {
   id: number;
@@ -137,19 +138,7 @@ export default function ReviewPage() {
         </div>
       </div>
 
-      <Modal.Root open={isLoginModalOpen} onOpenChange={setIsLoginModalOpen}>
-        <Modal.Content
-          title={<Modal.Title>로그인이 필요해요.</Modal.Title>}
-          bottomAffix={
-            <Modal.BottomAffix>
-              <Modal.Close label={"취소"} />
-              <Modal.Confirm label={"로그인"} onClick={() => router.push(PATH.LOGIN)} />
-            </Modal.BottomAffix>
-          }
-        >
-          코코스를 더 잘 즐기기 위해 로그인을 해주세요.
-        </Modal.Content>
-      </Modal.Root>
+      <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen} />
     </div>
   );
 }
