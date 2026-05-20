9e15f094c9d7c7a0d63cf20725bab670fb8f306b
yarimu <yhli0320@naver.com>
2026-05-21 00:24:23 +0900

    design: 세부 디자인 조정

--- src/shared/component/SearchHospital/SearchHospital.css.ts
+++ src/shared/component/SearchHospital/SearchHospital.css.ts
@@ -90,11 +90,11 @@ export const titleStyle = style([
 export const cardContainer = style({
   position: "absolute",
   top: "12rem",
-  bottom: "12.6rem",
+  bottom: "7.4rem",
 
   width: "100%",
   height: "auto",
-  maxHeight: "calc(100% - 24.6rem)",
+  maxHeight: "calc(100% - 19.4rem)",
   overflowY: "auto",
   paddingBottom: "1rem",
 
@@ -119,7 +119,7 @@ export const buttonContainer = style({
 
   width: "100%",
   height: "7.4rem",
-  padding: "0 2rem 3.2rem 2rem",
+  padding: "1.2rem 2rem 3.2rem 2rem",
 
   backgroundColor: color.gray.gray000,
 });
