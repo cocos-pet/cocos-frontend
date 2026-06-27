6ec4b70a0c06b7515f18178f02d3df7a584e498a
minjeoong <orange_mj@naver.com>
2026-06-27 18:12:10 +0900

    fix: site URL 수정

--- src/shared/constant/site.ts
+++ src/shared/constant/site.ts
@@ -1,4 +1,4 @@
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? "https://www.cocos.p-e.kr";
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? "https://www.cocos-pet.kr";
 
 export const siteConfig = {
   name: "COCOS",
@@ -7,7 +7,16 @@ export const siteConfig = {
   titleTemplate: "%s | COCOS",
   description:
     "COCOS(코코스)는 반려동물 증상 고민을 나누고, 동물병원 정보와 리뷰를 확인할 수 있는 반려동물 커뮤니티 서비스입니다.",
-  keywords: ["COCOS", "코코스", "반려동물", "펫 커뮤니티", "동물병원", "반려동물 증상", "반려동물 건강", "병원 리뷰"],
+  keywords: [
+    "COCOS",
+    "코코스",
+    "반려동물",
+    "펫 커뮤니티",
+    "동물병원",
+    "반려동물 증상",
+    "반려동물 건강",
+    "병원 리뷰",
+  ],
   url: SITE_URL,
   logoUrl: `${SITE_URL}/cocos2.svg`,
   locale: "ko_KR",
