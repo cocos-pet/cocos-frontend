cde4409c793c55844a9ae7628c4ab201bc4fd245
이윤지 <rhaxlddbswl3@gmail.com>
2026-09-07 23:31:20 +0900

    [Feat] 메인 캐러셀 이미지에 링크 이동 기능 추가
    - carouselImages에 href 필드 추가
    - href가 있을 경우 Link 컴포넌트로 감싸 같은 탭 이동 처리

--- src/app/main/_section/mainHeader/Carousel/Images.ts
+++ src/app/main/_section/mainHeader/Carousel/Images.ts
@@ -3,7 +3,7 @@ import _carousel2 from "@asset/image/_carousel2.png";
 import _carousel3 from "@asset/image/_carousel3.png";
 
 export const carouselImages = [
-  { id: "slide-1", src: _carousel1 },
-  { id: "slide-2", src: _carousel2 },
-  { id: "slide-3", src: _carousel3 },
+  { id: "slide-1", src: _carousel1, href: "https://www.cocos-pet.kr/community/335" },
+  { id: "slide-2", src: _carousel2, href: "https://www.cocos-pet.kr/community/337" },
+  { id: "slide-3", src: _carousel3, href: "https://www.cocos-pet.kr/community/336" },
 ];
